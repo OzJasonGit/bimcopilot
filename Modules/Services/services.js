5f3407c389f0de448a2bd4cdad0e6289353146fd
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-02-21 18:37:45 +0000

    updated service images_1

--- Modules/Services/services.js
+++ Modules/Services/services.js
@@ -79,14 +79,59 @@ const Servicesmain = ({ stories, firstStory }) => {
             <div id={styles.PORTFOLIO_TITLE}>
 
               <div id={styles.IMAGE_LAYER_1}>
-                <div class="rounded-xl ... drop-shadow-2xl ..." id={styles.P_IMAGE_1}>
+                <div class="rounded-xl ... drop-shadow-2xl ... shadow-black" id={styles.P_IMAGE_1}>
+                  <div id={styles.IMAGE_POST}>
+                    <Image
+                      src={stories[0].image}
+                      alt="Picture of the author"
+                      width={500}
+                      height={500}
+                      style={{
+                        position: "absolute",
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
               
-              <div id={styles.IMAGE_LAYER_2}>    
-                <div class="rounded-xl ... drop-shadow-2xl ..." id={styles.P_IMAGE_2}>                  
+              <div id={styles.IMAGE_LAYER_2}>  
+
+                <div class="rounded-xl ... drop-shadow-2xl ... shadow-black" id={styles.P_IMAGE_2}> 
+                  <div id={styles.IMAGE_POST}>
+                    <Image
+                      src={stories[1].image}
+                      alt="Picture of the author"
+                      width={500}
+                      height={500}
+                      style={{
+                        position: "absolute",
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        }}
+                      />
+                  </div>   
                 </div>
-                <div class="rounded-xl ... drop-shadow-2xl ..." id={styles.P_IMAGE_3}>                 
+
+
+                <div class="rounded-xl ... drop-shadow-2xl ... shadow-black" id={styles.P_IMAGE_3}>  
+                    <div id={styles.IMAGE_POST}>
+                    <Image
+                      src={stories[2].image}
+                      alt="Picture of the author"
+                      width={500}
+                      height={500}
+                      style={{
+                        position: "absolute",
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>               
                 </div>           
               </div>
 
