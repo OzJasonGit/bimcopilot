32ac8d0a80830c826f5480bf74d45f7bdc84a6b0
almightymoon <atharqulimoon@gmail.com>
2024-02-21 20:13:42 +0500

    testing

--- Modules/Blog/blog.js
+++ Modules/Blog/blog.js
@@ -123,7 +123,7 @@ const Blogsmain = ({ stories, firstStory }) => {
                   >
                   {story.title}
                     Sustainable, Richer Architects through Technology and
-                    Automation testing
+                    Automation 
                   </h1>
                   <br/>
                   <p class="text-xl ... text-stone-400 ... font-avant_garde_bold">
