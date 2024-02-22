44a8f492816749579243e33fc26a915c50567ec7
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-02-22 20:15:22 +0000

    update

--- Modules/Services/services.js
+++ Modules/Services/services.js
@@ -319,7 +319,7 @@ const Servicesmain = ({ stories, firstStory }) => {
 
 
               <div id={styles.WORKING_IMAGES}>  
-                <div class="border-8 border-stone-700 ... rounded-4xl ..." id={styles.HOW_WE_WORK_IMAGE}>
+                <div class="border-2 border-stone-400 ... rounded-3xl ..." id={styles.HOW_WE_WORK_IMAGE}>
                 </div>                 
               </div> 
 
