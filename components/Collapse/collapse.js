2803a0f325a2812ab6bf512eada4b035f934d242
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-02-22 19:37:46 +0000

    update

--- components/Collapse/collapse.js
+++ components/Collapse/collapse.js
@@ -53,7 +53,7 @@ const panel_4 = () => (
 
 
 const panel_5 = () => (
-    <h2 style={{ color: "white", alignItems: "end" }}
+    <h2 style={{ alignItems: "end" }}
         class="font-avant_garde_bold text-4xl text-stone-600 ..."
         id={styles.TEXT_OUTLINE_2}> 
         Revit Training 
@@ -92,8 +92,8 @@ const Collapsed = () => {
                                             key={"1"}
                                             header={panel_1()}
                                         >
-                                            <h3 class="font-avant_garde_medium text-xl ..."
-                                                style={{ color: "#f1f1f1", position: "relative", left: "25px" }}
+                                            <h3 class="font-avant_garde_medium text-2xl ... text-stone-600 ..."
+                                                style={{ position: "relative", left: "25px" }}
                                                 id={styles._H3}
                                             >
                                                 A dog is a type of domesticated
@@ -109,8 +109,8 @@ const Collapsed = () => {
                                             key={"2"}
                                             header={panel_2()}
                                         >
-                                            <h3 class="font-avant_garde_medium text-xl ..."
-                                                style={{ color: "#f1f1f1", position: "relative", left: "25px" }}
+                                            <h3 class="font-avant_garde_medium text-2xl ... text-stone-600 ..."
+                                                style={{ position: "relative", left: "25px" }}
                                                 id={styles._H3}
                                             >
                                                 A dog is a type of domesticated
@@ -125,8 +125,8 @@ const Collapsed = () => {
                                             key={"3"}
                                             header={panel_3()}
                                         >
-                                            <h3 class="font-avant_garde_medium text-xl ..."
-                                                style={{ color: "#f1f1f1", position: "relative", left: "25px" }}
+                                            <h3 class="font-avant_garde_medium text-2xl ... text-stone-600 ..."
+                                                style={{ position: "relative", left: "25px" }}
                                                 id={styles._H3}
                                             >
                                                 A dog is a type of domesticated
@@ -141,8 +141,8 @@ const Collapsed = () => {
                                             key={"4"}
                                             header={panel_4()}
                                         >
-                                            <h3 class="font-avant_garde_medium text-xl ..."
-                                                style={{ color: "#f1f1f1", position: "relative", left: "25px" }}
+                                            <h3 class="font-avant_garde_medium text-2xl ... text-stone-600 ..."
+                                                style={{ position: "relative", left: "25px" }}
                                                 id={styles._H3}
                                             >
                                                 A dog is a type of domesticated
