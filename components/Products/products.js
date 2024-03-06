ac7b7044dc1dbfccd6df0fc9d01a4ba5bcf5367a
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-03-06 16:34:52 +0000

    products updated

--- components/Products/products.js
+++ components/Products/products.js
@@ -30,86 +30,168 @@ export default class Products extends Component {
               id={styles.PRODUCTS_HOLDER_MOBILE}
               styles={{ position: "absolute" }}>
               <div id={styles.PRODUCTS_HORIZONTAL_MOBILE}>
-                  <div
-                      class="rounded-2xl ...  drop-shadow-2xl ..."
+
+
+
+              
+
+                  
+
+
+
+
+
+
+
+
+
+
+                <div
+                      class="rounded-md ..."
                       id={styles.PRODUCT_CARD}
-                      styles={{ gridArea: "1" }}>                         
-                  <div>
-                    <Link href="/">
-                      <h2
-                        id={styles._H2}
-                        class=" text-neutral-700 ... font-avant_garde_bold"
-                      >
-                        This is the product title
-                      </h2>
-                      <h3 class=" font-avant_garde_medium">
-                        {" "}
-                        This the product description
-                      </h3>
-                    </Link>
-                  </div>
+                      styles={{ gridArea: "2" }}>
+
+
+
+
+                      <div 
+                        id={styles.IMAGE_1}>
+                        
+                          <Image
+                            class=" rounded-md ... "
+                            id={styles.PRODUCT_IMAGE}
+                            src={revitBlinds}                    
+                            style={{objectFit: "cover"}}
+                            quality={100}
+                            //object-fit="cover"                      
+                            alt=""/>                                                                            
+                          <Image/>
+
+                      </div>
+                      
+
+
+
+                      <div>
+                        <Link href="/">
+                          <h2
+                            id={styles._H2}
+                            class=" text-neutral-700 ... font-avant_garde_bold">
+                            Parametric Title Block Catalogue (Metric + Imperial)
+                          </h2>
+                          <br/>
+                          <h4 class=" text-neutral-700 ... font-avant_garde_bold">
+                            {" "}
+                            From $53.90
+                          </h4>
+                        </Link>
+                      </div>
+
+
                 </div>
+
 
 
                 <div
-                  class="rounded-md ... drop-shadow-2xl ..."
-                  id={styles.PRODUCT_CARD}
-                  styles={{ gridArea: "2" }}>
-                  <Image
-                              id={styles.IMAGE_1}
-                              src={revitBlinds}                    
-                              style={{objectFit: "cover"}}
-                      	      quality={100}
-                              //object-fit="cover"                      
-                              alt=""/> 
-                              <Link
-                                  href="/"
-                                  id={styles.PRODUCT_IMAGE}
-                                  class="bg-neutral-800 rounded-xl ... drop-shadow-2xl ...">                             
-                              </Link>                             
-                        <Image/>
-                  <div>
-                    <Link href="/">
-                      <h2
-                        id={styles._H2}
-                        class=" text-neutral-700 ... font-avant_garde_bold"
-                      >
-                        This is the product title
-                      </h2>
-                      <h3 class=" text-neutral-700 ... font-avant_garde_medium">
-                        {" "}
-                        This the product description
-                      </h3>
-                    </Link>
-                  </div>
+                      class="rounded-md ..."
+                      id={styles.PRODUCT_CARD}
+                      styles={{ gridArea: "1" }}>
+
+
+
+
+                              <div 
+                                id={styles.IMAGE_1}>
+                                
+                                  <Image
+                                    class=" rounded-md ... "
+                                    id={styles.PRODUCT_IMAGE}
+                                    src={drawingSheets}                     
+                                    style={{objectFit: "cover"}}
+                                    quality={100}
+                                    //object-fit="cover"                      
+                                    alt=""/>                                                                            
+                                  <Image/>
+
+                              </div>
+                              
+
+
+
+                              <div>
+                                <Link href="/">
+                                  <h2
+                                    id={styles._H2}
+                                    class=" text-neutral-700 ... font-avant_garde_bold">
+                                    Parametric Title Block Catalogue (Metric + Imperial)
+                                  </h2>
+                                  <br/>
+                                  <h4 class=" text-neutral-700 ... font-avant_garde_bold">
+                                    {" "}
+                                    From $53.90
+                                  </h4>
+                                </Link>
+                              </div>
+
+
                 </div>
 
 
-                <div
-                  class="rounded-md ... drop-shadow-2xl ..."
-                  id={styles.PRODUCT_CARD}
-                  styles={{ gridArea: "3" }}
-                >
-                  <Link
-                    href="/"
-                    id={styles.PRODUCT_IMAGE}
-                    class="bg-neutral-800 rounded-md ... drop-shadow-2xl ..."
-                  ></Link>
-                  <div>
-                    <Link href="/">
-                      <h2
-                        id={styles._H2}
-                        class=" text-neutral-700 ... font-avant_garde_bold"
-                      >
-                        This is the product title
-                      </h2>
-                      <h3 class=" text-neutral-700 ... font-avant_garde_medium">
-                        {" "}
-                        This the product description
-                      </h3>
-                    </Link>
-                  </div>
+
+
+
+
+
+
+
+
+
+
+
+              <div
+                      class="rounded-md ..."
+                      id={styles.PRODUCT_CARD}
+                      styles={{ gridArea: "3" }}>
+
+
+
+
+                              <div 
+                                id={styles.IMAGE_1}>
+                                
+                                  <Image
+                                    class=" rounded-md ... "
+                                    id={styles.PRODUCT_IMAGE}
+                                    src={volumesByRoom}                     
+                                    style={{objectFit: "cover"}}
+                                    quality={100}
+                                    //object-fit="cover"                      
+                                    alt=""/>                                                                            
+                                  <Image/>
+
+                              </div>
+                              
+
+
+
+                              <div>
+                                <Link href="/">
+                                  <h2
+                                    id={styles._H2}
+                                    class=" text-neutral-700 ... font-avant_garde_bold">
+                                    Parametric Title Block Catalogue (Metric + Imperial)
+                                  </h2>
+                                  <br/>
+                                  <h4 class=" text-neutral-700 ... font-avant_garde_bold">
+                                    {" "}
+                                    From $53.90
+                                  </h4>
+                                </Link>
+                              </div>
+
+
                 </div>
+
 
 
 
