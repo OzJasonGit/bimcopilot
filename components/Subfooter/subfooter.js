ee4a53abd5da91439a06909f9dc5bbe95cd5deeb
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-06-02 00:10:39 +0400

    Astronaut Modified

--- components/Subfooter/subfooter.js
+++ components/Subfooter/subfooter.js
@@ -17,29 +17,38 @@ export default class Subfooter extends Component {
 
     <Provider>
 
+   
+
       <section
         id={styles.SHADOW_SECTION_ASTRONAUT}
-        class={styles.center_holder}>
+        class={styles.center_holder}
+      >
+        <div class={styles.grid_0}>
 
-        <div class={styles.grid_0_image_right}>
           <div
-            id={styles.ASTRONAUT_IMAGE_2}>
-
+            id={styles.ASTRONAUT_HOLDER}
+          >
+            <div id={styles.ASTRONAUT_3}>
               <Image
                   src={astronaut}
                   alt="Picture of the author"
                   width={500}
                   height={500}
                   style={{
-                    position: "absolute",
+                    position: "relative",
                     width: "100%",
                     height: "100%",
                     objectFit: "cover",
                   }}
                 />
+              
+            </div>
+            
           </div>
+
         </div>
-      </section>  
+
+      </section>
 
     </Provider>
 
