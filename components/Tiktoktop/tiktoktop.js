adaca86284007dbe254e309bea5ca7e4442a5fda
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-06-19 23:51:58 +0400

    tiktok updated cloned project

--- components/Tiktoktop/tiktoktop.js
+++ components/Tiktoktop/tiktoktop.js
@@ -6,9 +6,10 @@ import styles from './tiktoktop.module.css'
 
 import React, { Component } from 'react';
 import Link from "next/link";
+import Image from "next/image";
 
 import Tiktok from "./client/Tiktok/tiktok";
-
+import tiktoklogo from "./Tiktok_Logo_4.png";
 
 
 
@@ -34,28 +35,46 @@ export default class Tiktoktop extends Component {
       <section id={styles.SHADOW_SECTION_TIKTOK} class={styles.center_holder}>
         <div class={styles.grid_0_main}>
           <div id={styles.MAIN_TIKTOK_HOLDER}>
-            <div id={styles.ICON_HOLDER}>
+
+
+            
+            <div id={styles.ICON_HOLDER}>  
+
               <Link
                 href="/"
                 id={styles.ICON_TIKTOK}
-                class=" rounded-2xl ... drop-shadow-xl ... "
               >
-                <FontAwesomeIcon
-                  icon={faTiktok}
-                  id={styles.TIKTOK_FONT}
-                  class={styles.CENTER}
-                />
+                <div 
+                  id={styles.ICON_TIKTOK_2}>
+
+                  <Image
+                        src={tiktoklogo}
+                        alt="Picture of the author"
+                        width={500}
+                        height={500}
+                        style={{
+                          position: "absolute",
+                          width: "auto",
+                          height: "100%",
+                          justifyItems: "center",
+                          alignItems: "center",
+                          objectFit: "contain",
+                        }}
+                      />
+
+                </div>
+                
               </Link>
+
             </div>
+
+
             <div id={styles.TIKTOK_HOLDER}>
               <div
                 id={styles.TIKTOK}
                 class="rounded-2xl ... drop-shadow-2xl ... shadow-black"
               >
                 <Tiktok />
-                    <div className="tiktok-video">
-              <video autoPlay controls playsInline muted loop src="https://res.cloudinary.com/dytsuek4h/video/upload/v1712935614/ad4f40818468b4acb07956069ae103e3_cqoozt.mp4" style={{width:"100%", height:"100%"}}></video>
-              </div>
               </div>
             </div>
           </div>
