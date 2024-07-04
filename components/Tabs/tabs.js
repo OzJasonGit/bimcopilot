b68c9d5039997fdc938089ad63558b58277cb496
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-07-04 20:27:42 +0400

    update

--- components/Tabs/tabs.js
+++ components/Tabs/tabs.js
@@ -30,10 +30,10 @@ const Tab = () => {
   {
     key: `1`,
     src:  {logoMobile},
-    text: <h2 class="text-2xl ..." >` A dog is a type of domesticated animal.
+    text: <h1>` A dog is a type of domesticated animal.
                 Known for its loyalty and faithfulness,
                 it can be found as a welcome guest in many households across the world.`
-          </h2>,
+          </h1>,
     label:<div id={styles.T_TAB_HOLDER}>
             <div class="rounded-full ...   bg-indigo-500 ..." id={styles.T_TAB}>
             </div>
