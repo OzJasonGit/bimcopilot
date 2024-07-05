b1b7ffdf3c0d3100cd47f41eb3a54ac531d246b0
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-07-05 17:00:40 +0400

    update

--- components/Tabs/tabs.js
+++ components/Tabs/tabs.js
@@ -52,7 +52,7 @@ const Tab = () => {
                 Known for its loyalty and faithfulness,
               </h3>
             </a>
-            {''} {''}    <br/>  
+            {''} <br/>  
             ` Known for its loyalty and faithfulness,
                 it can be found as a welcome guest in many households across the world.`
 
