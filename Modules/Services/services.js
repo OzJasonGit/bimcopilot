bc4b1c5a14a25acad01b8d5acf107857c79a3940
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-07-23 21:26:58 +0400

    services updated

--- Modules/Services/services.js
+++ Modules/Services/services.js
@@ -38,7 +38,7 @@ const Servicesmain = ({ stories, firstStory }) => {
     
 
       <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
-          <div class={styles.grid_0_subscribe}>
+          <div class={styles.grid_0_scroll}>
             <div class={styles.sub_head}>
               <div id={styles.SUBSCRIBE} class={"border-emerald-200"}>
                 <div id={styles.SUB_ICON}>
