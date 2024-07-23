c5a8e935215ed8968bea78986d8302f524aa8fe7
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-07-23 22:07:09 +0400

    services update

--- Modules/Services/services.js
+++ Modules/Services/services.js
@@ -31,7 +31,7 @@ const Servicesmain = ({ stories, firstStory }) => {
 
 
       <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
-        <div class={styles.grid_0_subscribe}>
+        <div class={styles.grid_0_bimcopilot}>
           <div id={styles.BIMCOPILOT_CONTAINER}>
 
           </div>
