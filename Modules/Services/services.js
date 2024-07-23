ee332d74488d3d3f991bdd1bd90d8d293ac7dfa5
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-07-23 22:41:21 +0400

    bimcopilot

--- Modules/Services/services.js
+++ Modules/Services/services.js
@@ -36,11 +36,14 @@ const Servicesmain = ({ stories, firstStory }) => {
       <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
         <div class={styles.grid_0_bimcopilot}>
           <div id={styles.BIMCOPILOT_CONTAINER}>
-            <Image id={styles.CENTER}
-                                      src={bimcopilot}   
-                                      style={{objectFit: "contain"}} 
-                                      quality={100}
-                                />  
+            <div id={styles.BIMCOPILOT}>
+              <Image id={styles.CENTER}
+                  src={bimcopilot}   
+                  style={{objectFit: "contain"}} 
+                  quality={100}
+              />  
+            </div>
+            
 
           </div>
         </div>
