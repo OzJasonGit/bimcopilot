aa95d0f945ff711a8dd8381b3719a487e5417e6d
OzJasonGit <hello@plasticfreebydesign.com>
2024-07-29 23:46:36 +0500

    extra services has been commented

--- Modules/Services/services.js
+++ Modules/Services/services.js
@@ -16,14 +16,14 @@ import Infinite_Scroll_2 from "../../components/Carousel/Infinite_Scroll_Service
 
 import Services_1 from "../../components/services_1/services_1";
 import Services_2 from "../../components/services_2/services_2";
-import Services_3 from "../../components/services_3/services_3";
-import Services_4 from "../../components/services_4/services_4";
-import Services_5 from "../../components/services_5/services_5";
-import Services_6 from "../../components/services_6/services_6";
-import Services_7 from "../../components/services_7/services_7";
-import Services_8 from "../../components/services_8/services_8";
-import Services_9 from "../../components/services_9/services_9";
-import Services_10 from "../../components/services_10/services_10";
+// import Services_3 from "../../components/services_3/services_3";
+// import Services_4 from "../../components/services_4/services_4";
+// import Services_5 from "../../components/services_5/services_5";
+// import Services_6 from "../../components/services_6/services_6";
+// import Services_7 from "../../components/services_7/services_7";
+// import Services_8 from "../../components/services_8/services_8";
+// import Services_9 from "../../components/services_9/services_9";
+// import Services_10 from "../../components/services_10/services_10";
 
 
 
