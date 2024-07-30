8ee15c4e23bd522b50fb8fa0fe515c3821317ad7
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-07-30 23:02:06 +0400

    portfolio update

--- Modules/Services/services.js
+++ Modules/Services/services.js
@@ -170,50 +170,6 @@ const Servicesmain = ({ stories, firstStory }) => {
       <Services_6/>
       <Services_7/>
 
-
-
-
-
-
-
-
-      <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
-          <div class={styles.grid_0_how_we_work}>   
-           
-              <div id={styles.HOW_WE_WORK_TAG}>
-                  <h1                
-                    id={styles._H2_7XL}
-                    class="text-center ... text-6xl ... text-stone-700 ... font-avant_garde_bold"
-                  >         
-                    How we Work
-                  </h1>
-              </div>
-
-
-              <div id={styles.WORKING_IMAGES}>  
-                <div class="border-2 border-stone-600 ... rounded-2xl ..." id={styles.HOW_WE_WORK_IMAGE}>
-                </div>                 
-              </div> 
-
-
-
-                    
-          </div>
-      </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
           <div class={styles.grid_0_client_showcase}>
              
---
+++ components/services_8/services_8.js
@@ -0,0 +1,130 @@
+'use client'
+
+import styles from './services_8.module.css';
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+const Services_8 = ({ stories, firstStory }) => {
+const storiesToMap = stories.filter((story, i) => i != 0);
+const router = useRouter();
+
+return (
+
+    <>
+
+         <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
+          <div class={styles.grid_0_client_showcase}>
+             
+              
+
+              <div id={styles.PORTFOLIO_IMAGES}>  
+                <div class="" id={styles.PORTFOLIO_CLIENT}>
+
+
+
+                  <div class="border-2 border-stone-600 ... rounded-xl ..." id={styles.P1}>
+                 
+                  </div>
+
+
+                  <div class="rounded-xl ... " id={styles.P2}> 
+                    <div id={styles.IMAGE_POST}>
+                      <Image
+                        src={stories[1].image}
+                        alt="Picture of the author"
+                        width={500}
+                        height={500}
+                        style={{
+                          position: "absolute",
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </div>
+
+                  </div>
+
+                  <div class="" id={styles.P3}>   
+                    <div id={styles.CLIENT_TAG_1}>
+                        <h1                 
+                          id={styles._H2_7XL}
+                          class="text-left ... text-4xl ... text-stone-700 ... font-avant_garde_bold"
+                        >         
+                          Client <br/>
+                          Showcase
+                        </h1>
+                        <br/>
+                    </div>                 
+                  </div>
+
+                  <div class="border-2 border-stone-600 ... rounded-xl ..." id={styles.P4}>
+                    
+                  </div>
+
+                  <div class="rounded-xl ..." id={styles.P5}>
+                    <div id={styles.IMAGE_POST}>
+                      <Image
+                        src={stories[3].image}
+                        alt="Picture of the author"
+                        width={500}
+                        height={500}
+                        style={{
+                          position: "absolute",
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </div>
+                  </div>
+
+                  <div class="rounded-xl ..." id={styles.P6}>
+                    <div id={styles.IMAGE_POST}>
+                      <Image
+                        src={stories[2].image}
+                        alt="Picture of the author"
+                        width={500}
+                        height={500}
+                        style={{
+                          position: "absolute",
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </div>
+                  </div>
+
+                  <div class="rounded-xl ..." id={styles.P7}>
+                    <div id={styles.IMAGE_POST}>
+                      <Image
+                        src={stories[0].image}
+                        alt="Picture of the author"
+                        width={500}
+                        height={500}
+                        style={{
+                          position: "absolute",
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </div>
+                  </div>
+                 
+                </div>                   
+              </div> 
+     
+          </div>
+      </section>
+
+    </>
+
+);
+};
+
+
+export default Services_8;
