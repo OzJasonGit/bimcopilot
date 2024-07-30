3a28c0158016832687ab92443cb86c58700bc21b
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-07-30 22:44:45 +0400

    services 7 added

--- Modules/Services/services.js
+++ Modules/Services/services.js
@@ -20,7 +20,7 @@ import Services_3 from "../../components/services_3/services_3";
 //import Services_4 from "../../components/services_4/services_4";
 import Services_5 from "../../components/services_5/services_5";
 import Services_6 from "../../components/services_6/services_6";
-// import Services_7 from "../../components/services_7/services_7";
+import Services_7 from "../../components/services_7/services_7";
 // import Services_8 from "../../components/services_8/services_8";
 // import Services_9 from "../../components/services_9/services_9";
 // import Services_10 from "../../components/services_10/services_10";
@@ -168,62 +168,8 @@ const Servicesmain = ({ stories, firstStory }) => {
 
       <Services_5/>
       <Services_6/>
+      <Services_7/>
 
-      
-
-
-
-      <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
-          <div class={styles.grid_0_testimonials}> 
-             <div class={styles.testimonials}>
-              <div id={styles.SUBSCRIBE} class={"border-emerald-200"}>
-                <div id={styles.SUB_ICON}>
-                  
-                </div>
-
-                <div id={styles.MAIN_TAG}>
-                  <h1
-                   
-                    id={styles._H1}
-                    class="text-center ... text-6xl ... text-stone-700 ... font-avant_garde_bold" 
-                  >
-           
-                    <a class="text-6xl ...">" </a>Services <a class="text-6xl ..."> " </a>
-                  </h1>
-                </div>
-                <div id={styles.SUB_TAG}>
-                  <h3
-                    id={styles._H3}
-                    class="text-center ... text-2xl ... text-stone-500 ... font-avant_garde_bold"
-                  >
-                    {" "}
-                
-                    <a class="text-stone-700 ... font-avant_garde_bold">
-                      
-
-                    </a>{" "}
-                    The world is changing and so is{" "}
-                    <a class="text-stone-700 ... font-avant_garde_bold">
-                      architecture
-                    </a>
-                    . Discover new narratives, build better{" "}
-                    <a class="text-stone-700 ... font-avant_garde_bold">
-                      systems
-                    </a>
-                    , make more{" "}
-                    <a class="text-stone-700 ... font-avant_garde_bold">money</a>,
-                    be more{" "}
-                    <a class="text-stone-700 ... font-avant_garde_bold">
-                      sustainable
-                    </a>
-                    . <br /> <br />{" "}
-                  
-                  </h3>
-                </div>
-              </div>
-            </div>              
-          </div>
-      </section>
 
 
 
---
+++ components/services_7/services_7.js
@@ -0,0 +1,42 @@
+'use client'
+
+import styles from './services_7.module.css';
+import React, { Component } from 'react';
+
+
+
+export default class Services_6 extends Component {
+
+    render() {
+      
+    return (
+
+      <>
+
+       <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
+          <div class={styles.grid_0_how_we_work}>   
+           
+              <div id={styles.HOW_WE_WORK_TAG}>
+                  <h1                
+                    id={styles._H2_7XL}
+                    class="text-center ... text-6xl ... text-stone-700 ... font-avant_garde_bold"
+                  >         
+                    How we Work
+                  </h1>
+              </div>
+
+
+              <div id={styles.WORKING_IMAGES}>  
+                <div class="border-2 border-stone-600 ... rounded-2xl ..." id={styles.HOW_WE_WORK_IMAGE}>
+                </div>                 
+              </div> 
+                   
+          </div>
+      </section>
+    </>
+
+    )
+  };
+};
+
+
