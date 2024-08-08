bdb6405b3d9a62eb4ea334941c7ff9f615f9c6c3
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-08-08 18:43:29 +0400

    stop

--- Modules/Blog/blog.js
+++ Modules/Blog/blog.js
@@ -152,7 +152,7 @@ const Blogsmain = ({ stories, firstStory }) => {
 
       {storiesToMap.map((story, index) => {
         return (
-        <section id={styles.SHADOW_SECTION_TITLE} class={styles.center_holder}>
+        <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
           <div class={styles.grid_0_tag}>
             <div class={styles.sub_head}>
               <div id={styles.SUBSCRIBE} class={"border-emerald-200"}>
@@ -209,7 +209,7 @@ const Blogsmain = ({ stories, firstStory }) => {
 
     {storiesToMap.map((story, index) => {
         return (
-        <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
+        <section id={styles.SHADOW_SECTION_TITLE} class={styles.center_holder}>
           <div class={styles.grid_0_tag}>
             <div class={styles.sub_head}>
               <div id={styles.SUBSCRIBE} class={"border-emerald-200"}>
@@ -358,6 +358,301 @@ const Blogsmain = ({ stories, firstStory }) => {
       </div>
     </section>  
     )})};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {storiesToMap.map((story, index) => {
+                  return (
+      <section id={styles.SHADOW_SECTION_TITLE} class={styles.center_holder}>
+
+        <div class={styles.grid_0_main}>
+          <div id={styles.MAIN_TEXT_HOLDER}>
+
+
+
+            <div id={styles.SIDE_NAV_HOLDER}>
+              <div id={styles.SIDE_NAV} class="bg-cyan-500 ...">
+              </div>
+            </div>
+            
+            <div id={styles.DISCLAIMER_HOLDER}>
+              <p
+                class="text-xl ... text-neutral-700 ... font-avant_garde_bold">
+                {story.title}                             
+              </p>
+            </div>
+
+
+            <div id={styles.BLOG_HOLDER}>
+
+              <div id={styles.PARAGRAPH_1}>
+                <h3
+                  class="text-4xl ... text-neutral-700 ... font-avant_garde_bold"
+                  id={styles._H3}>
+                  {story.subtitle}                             
+                </h3>
+                <br/>
+                <p class="text-xl ... text-neutral-700 ... font-avant_garde_bold">
+                  {story.title}
+                  This is the paragraph Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+
+              <div class="bg-stone-900 ... rounded-xl ...">                
+              </div>
+             
+              <div id={styles.PARAGRAPH_2}>
+                <br/>
+                <br/>
+                <h3
+                  class="text-4xl ... text-neutral-700 ... font-avant_garde_bold"
+                  id={styles._H3}>
+                  This is the title section Lorem ipsum dolor sit amet, consectetur              
+                </h3>
+                <br/>
+                <p class="text-xl ... text-neutral-700 ... font-avant_garde_bold">
+               
+                  This is the paragraph Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </div>
+            
+            
+
+      </section>
+     )})};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              <div id={styles.PARAGRAPH_3}>
+                <br/>
+                <h3
+                  class="text-4xl ... text-neutral-700 ... font-avant_garde_bold"
+                  id={styles._H3}
+                >
+                 
+                  This is the title section Lorem ipsum dolor sit amet, consectetur              
+                </h3>
+                <br/>
+                <p class="text-xl ... text-neutral-700 ... font-avant_garde_bold">
+                
+                  This is the paragraph Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod. 
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+
+
+              <div id={styles.ADVERT_1} class="bg-yellow-300 ... rounded-xl ...">                
+              </div>
+
+
+              <div id={styles.PARAGRAPH_4}>
+                <br/>
+                <br/>
+                <h3
+                  class="text-4xl ... text-neutral-700 ... font-avant_garde_bold"
+                  id={styles._H3}
+                >
+                
+                  This is the title section Lorem ipsum dolor sit amet, consectetur              
+                </h3>
+                <br/>
+                <p class="text-xl ... text-neutral-700 ... font-avant_garde_bold">
+                
+                  This is the paragraph Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                </p>
+              </div>
+
+              <div id={styles.ADVERT_2} class="bg-blue-600 ... rounded-xl ...">                
+              </div>
+
+
+              <div id={styles.PARAGRAPH_5}>
+                <br/>
+                <br/>
+                <h3
+                  class="text-4xl ... text-neutral-700 ... font-avant_garde_bold"
+                  id={styles._H3}
+                >
+         
+                  This is the title section Lorem ipsum dolor sit amet, consectetur              
+                </h3>
+                <br/>
+                <p class="text-xl ... text-neutral-700 ... font-avant_garde_bold">
+              
+                  This is the paragraph Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                </p>
+              </div>
+
+
+              <div id={styles.CONCLUSION}>
+                <br/>
+                <h3
+                  class="text-4xl ... text-neutral-700 ... font-avant_garde_bold"
+                  id={styles._H3}
+                >
+            
+                  This is the title section Lorem ipsum dolor sit amet, consectetur              
+                </h3>
+                <br/>
+                <p class="text-xl ... text-neutral-700 ... font-avant_garde_bold">
+           
+                  This is the paragraph Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                </p>
+              </div>
+
+            </div>;          
+          </div>
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        )})};
+
+
+
+
 
  
     <Footer/>
