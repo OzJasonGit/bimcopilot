44755ff99930f2a1fa3d0b71890c56c7b82811fc
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-08-18 17:12:16 +0100

    updated

--- Modules/Blog/blog.js
+++ Modules/Blog/blog.js
@@ -36,8 +36,6 @@ const Blogsmain = ({ stories, firstStory }) => {
     {storiesToMap.map((story, index) => {
       return (
 
-
-
         <Head>
           <title>
             {story.title}
@@ -726,11 +724,50 @@ const Blogsmain = ({ stories, firstStory }) => {
 {storiesToMap.map((story, index) => {
         return (
         <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
+
           <div class={styles.grid_0_tag}>
+
+            <div class={styles.grid_0_blogimage_foot}>
+                  <div id={styles.BLOGIMAGE_HOLDER_FOOT}>
+                    
+                    <div id={styles.BLOGIMAGE_FOOT}>
+
+                      <div class="rounded-md ..." id={styles.B_IMAGE_FOOT}> 
+                        <Link href="/services">
+                            <Image
+                          alt="Picture of the author"
+                          key={story._id}
+                          width={500}
+                          height={500}
+                          src={story.image}
+                          style={{
+                          position: "absolute",
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                          }}/>
+                        </Link>                                                                                
+                      </div> 
+
+                      <div id={styles.BLOG_TEXT_FOOT}> 
+                        <h2 id={styles._H2} class="text-stone-700 ...  text-xl ... font-avant_garde_bold">
+                          {story.title}
+                        </h2>                                             
+                        <h3 id={styles._H3} class="text-stone-400 ... font-avant_garde_bold">
+                          {story.subtitle}
+                        </h3>
+                      </div>
+                                
+                </div>  
+              </div>              
+            </div>
+
 
 
             <div class={styles.sub_head}>
               <div id={styles.SUBSCRIBE} class={"border-emerald-200"}>
+
+
                 <div id={styles.SUB_ICON}>                 
                 </div>
          
@@ -749,48 +786,12 @@ const Blogsmain = ({ stories, firstStory }) => {
                     This is the paragraph Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor. 
                   </p>
-                </div>                
+                </div> 
+
               </div>
-
-
-
-              <div class={styles.grid_0_blogimage_foot}>
-                      <div id={styles.BLOGIMAGE_HOLDER_FOOT}>
-                       
-                        <div id={styles.BLOGIMAGE_FOOT}>
-
-                          <div class="rounded-md ..." id={styles.B_IMAGE_FOOT}> 
-                            <Link href="/services">
-                                <Image
-                              alt="Picture of the author"
-                              key={story._id}
-                              width={500}
-                              height={500}
-                              src={story.image}
-                              style={{
-                              position: "absolute",
-                              width: "100%",
-                              height: "100%",
-                              objectFit: "cover",
-                              }}/>
-                            </Link>                                                                                
-                          </div> 
-
-                          <div id={styles.BLOG_TEXT_FOOT}> 
-                            <h2 id={styles._H2} class="text-stone-700 ...  text-xl ... font-avant_garde_bold">
-                              {story.title}
-                            </h2>                                             
-                            <h3 id={styles._H3} class="text-stone-400 ... font-avant_garde_bold">
-                              {story.subtitle}
-                            </h3>
-                          </div>
-                                    
-                    </div>  
-                  </div>              
-                </div>
-
             </div>
           </div>
+
         </section>  
         )})}
 
