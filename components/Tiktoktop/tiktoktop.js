a0554f6df24d4688f5eeb844edecfb43a1521245
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-09-07 17:43:58 +0400

    status update

--- components/Tiktoktop/tiktoktop.js
+++ components/Tiktoktop/tiktoktop.js
@@ -68,7 +68,7 @@ export default class Tiktoktop extends Component {
 
 
                 <div 
-                  id={styles.ICON_TIKTOK_2}>
+                  id={styles.ICON_TIKTOK_3}>
                   <Image
                         src={tiktoklogo_dark}
                         alt="Picture of the author"
