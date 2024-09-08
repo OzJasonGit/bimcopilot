6ec652903b0feb888b917856dd0f6c90d5ebc669
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-09-08 20:42:09 +0400

    sales update

--- Modules/Saleslanding/saleslanding.js
+++ Modules/Saleslanding/saleslanding.js
@@ -36,7 +36,9 @@ const Saleslanding = ({ stories, firstStory }) => {
 
       <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
         <div class={styles.grid_0_scroll}>
+            <div id={styles.SALES_GRID_HOLDER}>
 
+            </div>
         </div>
       </section>
 
