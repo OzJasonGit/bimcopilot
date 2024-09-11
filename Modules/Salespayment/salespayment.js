ace54ae3747a9d4fe6b00380235434010c0a65c6
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-09-11 11:03:46 +0400

    zection added

--- Modules/Salespayment/salespayment.js
+++ Modules/Salespayment/salespayment.js
@@ -252,6 +252,12 @@ const Salespayment = ({ stories, firstStory }) => {
         </div>
       </section>
 
+      <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
+        <div class={styles.grid_0_scroll}>
+
+        </div>
+      </section>
+
 
 
     
