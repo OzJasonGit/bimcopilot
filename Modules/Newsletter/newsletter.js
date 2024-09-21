f44cc74a48519f1cca03d21a9fe8ca1568bcdd8f
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-09-21 10:43:45 +0400

    update

--- Modules/Newsletter/newsletter.js
+++ Modules/Newsletter/newsletter.js
@@ -74,6 +74,7 @@ const Saleslanding = ({ stories, firstStory }) => {
                                     width: "100%",
                                     height: "100%",
                                     objectFit: "cover",
+                                    top: "-100px"
                                     }}/>                        
                                 )})} 
                             </div>
