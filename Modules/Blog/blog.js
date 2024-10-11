1806e7bb760b9cbc86028c1d6e4ba44df2c0177f
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-10-11 17:43:33 +0400

    title

--- Modules/Blog/blog.js
+++ Modules/Blog/blog.js
@@ -23,7 +23,7 @@ import logoImage from "./Bim-copilot-logo_2.png";
 
 const Blogsmain = ({ stories, firstStory }) => {
   const params = useParams();
-  const storiesToMap = stories.filter((story, i) => story._id == params.slug);
+  const storiesToMap = stories.filter((story, i) => story.title == params.slug);
   const router = useRouter();
 
   
