5f84f998bddfee3d35e83feb6b8488513336abba
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-17 09:08:56 +0400

    busineesses

--- components/services_2/services_2.js
+++ components/services_2/services_2.js
@@ -23,7 +23,7 @@ import Placeholder_2 from './placeholder_2.jpeg';
 
 
 
-import { TrendingUp } from "lucide-react"
+/*import { TrendingUp } from "lucide-react"
 import { CartesianGrid, LabelList, Line, LineChart, XAxis } from "recharts"
 
 import {
@@ -51,7 +51,7 @@ const chartData = [
   { month: "April", desktop: 73, mobile: 190 },
   { month: "May", desktop: 209, mobile: 130 },
   { month: "June", desktop: 214, mobile: 140 },
-]
+]*/
 
 
 
@@ -95,69 +95,6 @@ return (
                         objectFit: "cover",
                       }}
                     />
-
-
-
-
-                    <Card>
-                      <CardHeader>
-                        <CardTitle>Line Chart - Label</CardTitle>
-                        <CardDescription>January - June 2024</CardDescription>
-                      </CardHeader>
-                      <CardContent>
-                        <ChartContainer config={chartConfig}>
-                          <LineChart
-                            accessibilityLayer
-                            data={chartData}
-                            margin={{
-                              top: 20,
-                              left: 12,
-                              right: 12,
-                            }}
-                          >
-                            <CartesianGrid vertical={false} />
-                            <XAxis
-                              dataKey="month"
-                              tickLine={false}
-                              axisLine={false}
-                              tickMargin={8}
-                              tickFormatter={(value) => value.slice(0, 3)}
-                            />
-                            <ChartTooltip
-                              cursor={false}
-                              content={<ChartTooltipContent indicator="line" />}
-                            />
-                            <Line
-                              dataKey="desktop"
-                              type="natural"
-                              stroke="var(--color-desktop)"
-                              strokeWidth={2}
-                              dot={{
-                                fill: "var(--color-desktop)",
-                              }}
-                              activeDot={{
-                                r: 6,
-                              }}
-                            >
-                              <LabelList
-                                position="top"
-                                offset={12}
-                                className="fill-foreground"
-                                fontSize={12}
-                              />
-                            </Line>
-                          </LineChart>
-                        </ChartContainer>
-                      </CardContent>
-                      <CardFooter className="flex-col items-start gap-2 text-sm">
-                        <div className="flex gap-2 font-medium leading-none">
-                          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                        </div>
-                        <div className="leading-none text-muted-foreground">
-                          Showing total visitors for the last 6 months
-                        </div>
-                      </CardFooter>
-                    </Card>
                   </div>      
               </div>
 
