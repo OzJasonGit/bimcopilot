6999e57185d2e1a20a15a2ab2b31b34c4d692884
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-18 20:27:06 +0400

    service update

--- components/services_10/services_10.js
+++ components/services_10/services_10.js
@@ -29,51 +29,58 @@ return (
                   <div class="rounded-xl ..."id={styles.WHO_WEVE_HELPED_TEXT}> 
 
                       <div id={styles.WHO_WEVE_HELPED_HOLDER}>
+
+
                         <div id={styles.WE_HELPED_TEXT}>
-
                           <div id={styles.ABOUT_TITLE_HOLDER}>
-                          <h1
-                            id={styles._H1}
-                            class="text-stone-700 ... font-avant_garde_bold ..."
-                          >
-                            <a class="text-left ... text-4xl">My background and who <br/> I've helped.</a>
-                          </h1>
-                        </div>
-                 
-                        
-
-                        <div
-                          class="content-center ..."
-                          id={styles.MY_FACE_HOLDER}
-                        >
-                        
-                        <div>
-                          <h3
-                              id={styles._H3}
-                              class="text-xl ... text-stone-700 ... font-avant_garde_bold ..."
-                            >
-                              Hi, I'm Oz Jason. I'm a registered architect, BIM
-                              manager and the founder of{" "}
-                              <a class="text-yellow-200 ...">bimcopilot.com</a>.
-                              <br/> However, I'm a firm believer that titles are
-                              becoming less important. What matters is how I can
-                              help you and your business. And I think <br /> I could
-                              help you a lot.
-                            </h3>
-                        </div>
-                            
-
-                        <br/>
-                        <br/>
-
-                        <div id={styles.MY_FACE} class=" border-8 border-stone-200 ... rounded-full ...">
-                            <Image></Image>
+                            <h1
+                              id={styles._H1}
+                              class="text-stone-700 ... font-avant_garde_bold ...">
+                              <a class="text-left ... text-4xl">My background and who <br/> I've helped.</a>
+                            </h1>
                           </div>
-                        </div>
-                        
-                    
-                   
+                                         
+                          <div
+                            class="content-center ..."
+                            id={styles.MY_FACE_HOLDER}>  
 
+                            <div>
+                              <h2
+                                  id={styles._H2}
+                                  class="text-xl ... text-stone-700 ... font-avant_garde_bold ...">
+                                  Hi, I'm Oz Jason. I'm a registered architect, BIM
+                                  manager and the founder of{" "}
+                                  <a class="text-yellow-200 ...">bimcopilot.com</a>.
+                                  <br/> However, I'm a firm believer that titles are
+                                  becoming less important. What matters is how I can
+                                  help you and your business. 
+                              </h2>
+                            </div>
+
+                            <br/>
+                            <br/>
+
+                            <div>
+                              <h3
+                                  id={styles._H3}
+                                  class="text-xl ... text-stone-700 ... font-avant_garde_bold ...">
+                                  Hi, I'm Oz Jason. I'm a registered architect, BIM
+                                  manager and the founder of{" "}
+                                  <a class="text-yellow-200 ...">bimcopilot.com</a>.
+                                  <br/> However, I'm a firm believer that titles are
+                                  becoming less important. What matters is how I can
+                                  help you and your business. 
+                              </h3>
+                            </div>
+                            
+                            <br/>
+                            <br/>
+
+                            <div id={styles.MY_FACE} class=" border-8 border-stone-200 ... rounded-full ...">
+                                <Image></Image>
+                              </div>
+                            </div>
+                      
                         </div>                       
                       </div>
                                                                                     
@@ -85,24 +92,32 @@ return (
       </section>
 
 
+
+
+
+
+
+
+
+
+
       <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
           <div class={styles.grid_0_faq}>
              
-              
-
               <div id={styles.FAQ_BLOCK}>   
 
-                <div  id={styles.FAQ}>
-                  
+                <div  id={styles.FAQ}>                
                   <div class="border-2 border-stone-600 ... rounded-xl ..."id={styles.FAQ_TEXT}>                                        
                   </div>
-
                 </div>  
 
               </div> 
      
           </div>
       </section>
+
+
+
 
 
 
