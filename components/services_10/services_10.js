2ed27c6a1b6161e18b9bac3b70f1afba846c2d25
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-19 02:02:22 +0400

    updated

--- components/services_10/services_10.js
+++ components/services_10/services_10.js
@@ -101,7 +101,7 @@ return (
                     <h1
                       id={styles._H1}
                       class="text-stone-700 ... font-avant_garde_bold ...">
-                      <a class="text-left ... text-4xl">Frequently Asked Questions (FAQ) </a>
+                      <a class="text-left ... text-4xl">Frequently Asked <br/> Questions (FAQ) </a>
                     </h1>                                       
                   </div>
                 </div>  
