a891c81cf7a426178ab5e5d2af3de7086bd9fa2f
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-20 17:13:32 +0400

    pushed

--- components/services_2/services_2.js
+++ components/services_2/services_2.js
@@ -84,7 +84,7 @@ return (
 
               <div class="rounded-xl ... drop-shadow-2xl ... shadow-black" id={styles.P_IMAGE_1}>
                   <div id={styles.IMAGE_POST}>
-                    <div style={{position: "relative"}}>
+                    <div style={{position: "absolute", width:"100%", height:"100%" }}>
                       <Card className="flex flex-col">
                       <CardHeader className="items-center pb-0">
                         <CardTitle>Radial Chart - Grid</CardTitle>
@@ -127,7 +127,7 @@ return (
 
 
               <div class="rounded-xl ... " id={styles.P_IMAGE_2}> 
-                <div id={styles.IMAGE_POST}>
+                <div id={styles.IMAGE_POST_2}>
 
                   <Image
                       src={Placeholder_1}   
