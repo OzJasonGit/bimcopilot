55e7fbb3e3121beadec2b7730a453eb98a46e400
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-20 15:36:16 +0400

    burping

--- components/services_2/services_2.js
+++ components/services_2/services_2.js
@@ -170,72 +170,11 @@ return (
 
 
 
- <CardContent>
-        <ChartContainer config={chartConfig}>
-          <LineChart
-            accessibilityLayer
-            data={chartData}
-            margin={{
-              top: 20,
-              left: 12,
-              right: 12,
-            }}>
-
-            <CartesianGrid vertical={false} />
-
-            <XAxis
-              dataKey="month"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              tickFormatter={(value) => value.slice(0, 3)}/>
-
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent indicator="line" />}/>
-
-          </LineChart>
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <Line
-              dataKey="desktop"
-              type="natural"
-              stroke="var(--color-desktop)"
-              strokeWidth={2}
-              dot={{
-                fill: "var(--color-desktop)",
-              }}
-              activeDot={{
-                r: 6,
-              }}
-            >
-              <LabelList
-                position="top"
-                offset={12}
-                className="fill-foreground"
-                fontSize={12}/>
-            </Line>
-          </LineChart>
-        </ChartContainer>
-  </CardContent>
 
 
 
