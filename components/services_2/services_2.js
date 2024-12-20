84a5db2550ffe6251e65b4d3b37ca068a2c3d5a6
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-20 16:07:49 +0400

    radial2

--- components/services_2/services_2.js
+++ components/services_2/services_2.js
@@ -91,10 +91,13 @@ return (
                         <CardTitle>Radial Chart - Grid</CardTitle>
                         <CardDescription>January - June 2024</CardDescription>
                       </CardHeader>
+
+
+
                       <CardContent className="flex-1 pb-0">
                         <ChartContainer
                           config={chartConfig}
-                          className="mx-auto aspect-square max-h-[250px]"
+                          className="mx-auto aspect-square max-h-[100%]"
                         >
                           <RadialBarChart data={chartData} innerRadius={30} outerRadius={100}>
                             <ChartTooltip
@@ -106,6 +109,8 @@ return (
                           </RadialBarChart>
                         </ChartContainer>
                       </CardContent>
+
+
                       <CardFooter className="flex-col gap-2 text-sm">
                         <div className="flex items-center gap-2 font-medium leading-none">
                           Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
