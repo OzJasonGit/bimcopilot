67ee71907b9e84b7985d8bfca068eb0ebe029732
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-20 13:51:44 +0400

    charts

--- components/services_2/services_2.js
+++ components/services_2/services_2.js
@@ -46,7 +46,7 @@ const chartConfig = {
     label: "Mobile",
     color: "hsl(var(--chart-2))",
   },
-} satisfies ChartConfig
+}
 
 
 
@@ -169,67 +169,6 @@ return (
           </div>
         </section>
 
-
-
-
-
-
-
-        {/*<section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
-          <div class={styles.grid_0_scroll}>      
-            <div id={styles.PORTFOLIO_TITLE}>
-
-              <div id={styles.IMAGE_LAYER_1}>
-
-                <div class="rounded-xl ... drop-shadow-2xl ... shadow-black" id={styles.P_IMAGE_1}>
-                  <div id={styles.IMAGE_POST}>
-                    <Image
-                      src={Placeholder_1}   
-                      alt="Picture of the author"
-                      width={500}
-                      height={500}
-                      style={{
-                        position: "absolute",
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                      }}
-                    />
-                  </div>
-                  
-
-
-
-
-
-
-
-
-                </div>
-              </div>
-              
-              {/*<div id={styles.IMAGE_LAYER_2}>  
-                <div class="rounded-xl ... drop-shadow-2xl ... shadow-black" id={styles.P_IMAGE_2}> 
-                  <div id={styles.IMAGE_POST}>
-                    <Image
-                      src={Placeholder_2}   
-                      alt="Picture of the author"
-                      width={500}
-                      height={500}
-                      style={{
-                        position: "absolute",
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                        }}
-                      />
-                  </div>   
-                </div>                       
-              </div>
-
-            </div>          
-          </div>
-      </section> */}
     </>
 
 
