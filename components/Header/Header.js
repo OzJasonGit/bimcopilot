7bfd887b9c164c87759f570bf3e80fc56cfc2324
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-22 13:12:30 +0400

    I don't want to

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -31,91 +31,7 @@ export default class Header extends Component {
             <div class={styles.HEADER_HOLDER}>
               <div id={styles.header}>
                 <header id={styles.FIXED_HEADER}>
-                  <div class="content-center ..." id={styles.COPILOT_HOLDER}>
-                    <Link
-                      class="content-center ..."
-                      id={styles.COPILOT}
-                      href="/dashboard"
-                    >
-                      <Image
-                        src={logoMobile}
-                        alt="Picture of the author"
-                        width={500}
-                        height={500}
-                        style={{
-                          position: "absolute",
-                          width: "auto",
-                          height: "100%",
-                          justifyItems: "center",
-                          alignItems: "center",
-                          objectFit: "contain",
-                        }}
-                      />
-                    </Link>
-                  </div>
 
-                  
-
-                  <div class="content-center ..." id={styles.NAV_MENU}>
-                    <Link
-                      class="content-center ..."
-                      id={styles.SERVICES}
-                      href="/services"
-                    >
-                      <h3
-                        id={styles.H_3_SERVICES}
-                        class="text-center ...  text-slate-50 font-avant_garde_bold"
-                        data-content="Services"
-                      >
-                        Services
-                      </h3>
-                    </Link>
-
-                    <Link id={styles.PRODUCTS} href="/products">
-                      <h3
-                        id={styles.H_3_PRODUCTS}
-                        class="text-center ...  text-slate-50  font-avant_garde_bold"
-                        data-content="Shop"
-                      >
-                        Shop
-                      </h3>
-                    </Link>
-
-                    <Link id={styles.PROJECTS} href="/projects">
-                      <h3
-                        id={styles.H_3_PROJECTS}
-                        class="text-center ...  text-slate-50  font-avant_garde_bold"
-                        data-content="Projects"
-                      >
-                        Projects
-                      </h3>
-                    </Link>
-
-                    <Link id={styles.STORIES} href="/bloghome">
-                      <h3
-                        id={styles.H_3_STORIES}
-                        class="text-center ...  text-slate-50 font-avant_garde_bold"
-                        data-content="Stories"
-                      >
-                        Stories
-                      </h3>
-                    </Link>
-                  </div>
-
-                  <div class="content-center ..." id={styles.TIME}>
-
-                    <Clock />
-                   
-
-                    <div class="bg-zinc-500 ..." id={styles.B1}>
-                    </div>
-
-                    <DateComponent />
-
-                    
-
-                    
-                  </div>
 
                   <div id={styles.LOGO_ICON} class={styles.container}>
                     <Link id={styles.LOGO_LINK} href="/.">
@@ -137,6 +53,112 @@ export default class Header extends Component {
                       </div>
                     </Link>
                   </div>
+
+
+
+                  <div class="content-center ..." id={styles.COPILOT_HOLDER}>
+                    <Link
+                      class="content-center ..."
+                      id={styles.COPILOT}
+                      href="/dashboard">
+                      <Image
+                        src={logoMobile}
+                        alt="Picture of the author"
+                        width={500}
+                        height={500}
+                        style={{
+                          position: "absolute",
+                          width: "auto",
+                          height: "100%",
+                          justifyItems: "center",
+                          alignItems: "center",
+                          objectFit: "contain",
+                        }}
+                      />
+                    </Link>
+                  </div>
+
+                  
+
+
+
+
+                  <div class="content-center ..." id={styles.NAV_MENU}>
+                    <Link
+                      class="content-center ..."
+                      id={styles.SERVICES}
+                      href="/services">
+                      <h3
+                        id={styles.H_3_SERVICES}
+                        class="text-center ...  text-slate-50 font-avant_garde_bold"
+                        data-content="Services">
+                        Services
+                      </h3>
+                    </Link>
+
+                    <Link id={styles.PRODUCTS} href="/products">
+                      <h3
+                        id={styles.H_3_PRODUCTS}
+                        class="text-center ...  text-slate-50  font-avant_garde_bold"
+                        data-content="Shop">
+                        Shop
+                      </h3>
+                    </Link>
+
+                    <Link id={styles.PROJECTS} href="/projects">
+                      <h3
+                        id={styles.H_3_PROJECTS}
+                        class="text-center ...  text-slate-50  font-avant_garde_bold"
+                        data-content="Projects">
+                        Projects
+                      </h3>
+                    </Link>
+
+                    <Link id={styles.STORIES} href="/bloghome">
+                      <h3
+                        id={styles.H_3_STORIES}
+                        class="text-center ...  text-slate-50 font-avant_garde_bold"
+                        data-content="Stories">
+                        Stories
+                      </h3>
+                    </Link>
+                  </div>
+
+
+
+
+
+
+                  <div class="content-center ..." id={styles.TIME}>
+                    <Clock />                   
+                    <div class="bg-zinc-500 ..." id={styles.B1}>
+                    </div>
+                    <DateComponent />                              
+                  </div>
+
+
+
+
+                  <div class="content-right ..." id={styles.BASKET_CONTAINER}>
+
+                    <div>
+
+                    </div>
+
+                    <div>
+                      
+                    </div>
+
+                  </div>
+
+
+
+
+
+                  
+
+
+
                 </header>
               </div>
             </div>
