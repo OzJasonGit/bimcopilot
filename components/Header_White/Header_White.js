55ddae29e280608382c83a9323e80839753981d5
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-23 02:00:45 +0400

    high tax

--- components/Header_White/Header_White.js
+++ components/Header_White/Header_White.js
@@ -6,6 +6,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFaceRelieved,
   faBasketball,
+  faBasketShopping,
+  faCartShopping,
+  faHeart
+
+
 } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Header_White.module.css";
 import React, { Component } from "react";
@@ -13,7 +18,7 @@ import React, { Component } from "react";
 import Script from "next/script";
 
 import logoMobile from "./Bim-copilot-logo_Mobile_3.png";
-import bimcopilot_icon from "./Tesseract_Logo_Black.png";
+import bimcopilot_icon from "./Tesseract_Logo_2.png";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -22,75 +27,61 @@ import DateComponent from "./Clock/date";
 
 
 
-export default class Header_White extends Component {
+export default class Header extends Component {
   render() {
     return (
       <>
         <Provider>
           <section id={styles.SHADOW_SECTION} class={styles.center_holder}>
             <div class={styles.HEADER_HOLDER}>
-
-
-              <div id={styles.LOGO_ICON} class={styles.container}>
-                <Link id={styles.LOGO_LINK} href="/.">
-                  <div class=" " id={styles.LOGO}>
-                    <Image
-                      src={bimcopilot_icon}
-                      alt="Picture of the author"
-                      width={500}
-                      height={500}
-                      style={{
-                        position: "relative",
-                        width: "auto",
-                        height: "100%",
-                        justifyItems: "center",
-                        alignItems: "center",
-                        objectFit: "contain",
-                      }}
-                    />
-                  </div>
-                </Link>
-              </div>
-
-
               <div id={styles.header}>
+
+
+
+
+
                 <header id={styles.FIXED_HEADER}>
-                  <div class="content-center ..." id={styles.COPILOT_HOLDER}>
-                    <Link
-                      class="content-center ..."
-                      id={styles.COPILOT}
-                      href="/dashboard"
-                    >
-                      <Image
-                        src={logoMobile}
-                        alt="Picture of the author"
-                        width={500}
-                        height={500}
-                        style={{
-                          position: "absolute",
-                          width: "auto",
-                          height: "100%",
-                          justifyItems: "center",
-                          alignItems: "center",
-                          objectFit: "contain",
-                        }}
-                      />
+
+
+                  <div id={styles.LOGO_ICON} class={styles.container}>
+                    <Link id={styles.LOGO_LINK} href="/.">
+                      <div class=" " id={styles.LOGO}>
+                        <Image
+                          src={bimcopilot_icon}
+                          alt="Picture of the author"
+                          width={500}
+                          height={500}
+                          style={{
+                            position: "relative",
+                            width: "auto",
+                            height: "100%",
+                            justifyItems: "center",
+                            alignItems: "center",
+                            objectFit: "contain",
+                          }}
+                        />
+                      </div>
                     </Link>
                   </div>
 
+
+
+                 
+
                   
+
+
+
 
                   <div class="content-center ..." id={styles.NAV_MENU}>
                     <Link
                       class="content-center ..."
                       id={styles.SERVICES}
-                      href="/services"
-                    >
+                      href="/services">
                       <h3
                         id={styles.H_3_SERVICES}
-                        class="text-center ...  text-stone-700 font-avant_garde_bold"
-                        data-content="Services"
-                      >
+                        class="text-center ...  text-slate-50 font-avant_garde_bold"
+                        data-content="Services">
                         Services
                       </h3>
                     </Link>
@@ -98,9 +89,8 @@ export default class Header_White extends Component {
                     <Link id={styles.PRODUCTS} href="/products">
                       <h3
                         id={styles.H_3_PRODUCTS}
-                        class="text-center ...  text-stone-700  font-avant_garde_bold"
-                        data-content="Shop"
-                      >
+                        class="text-center ...  text-slate-50  font-avant_garde_bold"
+                        data-content="Shop">
                         Shop
                       </h3>
                     </Link>
@@ -108,9 +98,8 @@ export default class Header_White extends Component {
                     <Link id={styles.PROJECTS} href="/projects">
                       <h3
                         id={styles.H_3_PROJECTS}
-                        class="text-center ...  text-stone-700  font-avant_garde_bold"
-                        data-content="Projects"
-                      >
+                        class="text-center ...  text-slate-50  font-avant_garde_bold"
+                        data-content="Projects">
                         Projects
                       </h3>
                     </Link>
@@ -118,9 +107,8 @@ export default class Header_White extends Component {
                     <Link id={styles.STORIES} href="/bloghome">
                       <h3
                         id={styles.H_3_STORIES}
-                        class="text-center ...  text-stone-700 font-avant_garde_bold"
-                        data-content="Stories"
-                      >
+                        class="text-center ...  text-slate-50 font-avant_garde_bold"
+                        data-content="Stories">
                         Stories
                       </h3>
                     </Link>
@@ -132,23 +120,46 @@ export default class Header_White extends Component {
 
 
                   <div class="content-center ..." id={styles.TIME}>
-                    <Clock />                  
+                    <Clock />                   
                     <div class="bg-zinc-500 ..." id={styles.B1}>
                     </div>
-                    <DateComponent />                  
+                    <DateComponent />                              
                   </div>
 
-                  
 
 
 
                   <div class="content-right ..." id={styles.BASKET_CONTAINER}>
-                    <div class="content-center ... align-middle ...">
+
+                    <div class="content-center ... align-middle ..."
+                         style={{gridArea:"BASKET", position:"relative", top:"0.75px", left:"-30px"}}>
+                      {/*<FontAwesomeIcon icon={faHeart}  size="xs"
+                      class="text-slate-50"/>*/}
+
+
+
+                      <FontAwesomeIcon icon={faCartShopping} size="xs"
+                      class="text-slate-50" />
+                    </div>
+
+                    <div class="content-center ... align-middle ..."
+                         style={{gridArea:"FAVOURITE", position:"relative", top:"0.5px", left:"-30px"}}>
+                      <FontAwesomeIcon icon={faHeart}  size="2xs"
+                      class="text-slate-50"/>
                     </div>
 
                     <div class="content-center ... align-middle ...">
                     </div>
+
                   </div>
+
+
+
+
+
+                  
+
+
 
                 </header>
               </div>
