5cfdf9f0cfef43714526134ee5d3e6d10356fbb8
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-22 19:24:51 +0400

    broke

--- components/Header/Clock/date.js
+++ components/Header/Clock/date.js
@@ -13,7 +13,7 @@ class DateComponent extends Component {
   render() {
     return (
       <div id={styles.DATE}>
-        <h3 class="text-center ...  text-slate-50 font-avant_garde_bold"
+        <h3 id={styles._H3} class="text-center ...  text-slate-50 font-avant_garde_bold"
         style={{marginBottom:"0px"}}>{this.state.date}</h3>
       </div>
     );
