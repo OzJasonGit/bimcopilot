99c3929d1e3505abbf48a722a10916f6730837f0
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-25 23:15:52 +0400

    IFRAME

--- Modules/Copilot_Dashboard/copilot_dashboard.js
+++ Modules/Copilot_Dashboard/copilot_dashboard.js
@@ -54,7 +54,8 @@ const Copilot_Dashboard = ({ stories, firstStory }) => {
                      gridArea: "IMAGE",
                      overflow: "hidden",  
                      }}>
-                    <div style={{
+                    <iframe src="https://speckle.xyz/embed?stream=8c614e6658&commit=31247f8195&transparent=true&autoload=true&hidecontrols=true&hidesidebar=true&hideselectioninfo=true&commentslideshow=true" width="100%" height="100%" frameborder="0"></iframe>   {/* src={rocketShip}
+                    {/*<div style={{
                         position: "relative",
                         width: "100%",
                         height: "700px",                                      
@@ -74,7 +75,7 @@ const Copilot_Dashboard = ({ stories, firstStory }) => {
                                 objectFit: "cover",
                                 }}/>              
                             )})}
-                    </div>
+                    </div>*/}
                 </div>
 
 
