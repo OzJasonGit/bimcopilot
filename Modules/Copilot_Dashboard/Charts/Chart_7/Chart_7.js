50788911307baf7dc7a95ad42f0349bbd9b93743
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-27 15:27:48 +0400

    constant

--- Modules/Copilot_Dashboard/Charts/Chart_7/Chart_7.js
+++ Modules/Copilot_Dashboard/Charts/Chart_7/Chart_7.js
@@ -1,46 +1,57 @@
 import React, { Component } from "react";
-import { Line } from "react-chartjs-2"; // Import Line from react-chartjs-2
+import { Radar } from "react-chartjs-2"; // Import Line from react-chartjs-2
 import {
     Chart as ChartJS,
     LineElement,
+    PointElement,
     Tooltip,
     Legend,
-    CategoryScale,
-    LinearScale,
+    RadialLinearScale,
     PointElement,
 } from "chart.js";
 
 import styles from './Chart_7.module.css';
 
 // Register Chart.js components
-ChartJS.register(LineElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement);
+ChartJS.register(LineElement, PointElement, Tooltip, Legend, RadialLinearScale, PointElement);
 
 export default class Chart_7 extends Component {
     render() {
+
+
         const data = {
-            labels: ['January', 'February', 'March', 'April'], // X-axis labels
-            datasets: [
-                {
-                    label: 'Dataset 1',
-                    data: [10, 20, 30, 40], // Values for each label
-                    borderColor: 'rgba(75, 192, 192, 1)', // Line color
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)', // Fill color
-                    borderWidth: 2,
-                    tension: 0.4, // Smoothness of the curve (0 = sharp, 1 = very smooth)
-                    pointRadius: 5, // Radius of points on the line
-                    pointBackgroundColor: 'rgba(75, 192, 192, 1)', // Point color
-                },
-                {
-                    label: 'Dataset 2',
-                    data: [15, 25, 35, 45], // Another line
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderWidth: 2,
-                    tension: 0.4,
-                    pointRadius: 5,
-                    pointBackgroundColor: 'rgba(255, 99, 132, 1)',
-                },
+            labels: [
+                'Eating',
+                'Drinking',
+                'Sleeping',
+                'Designing',
+                'Coding',
+                'Cycling',
+                'Running'
             ],
+    
+           datasets: [{
+                label: 'My First Dataset',
+                data: [65, 59, 90, 81, 56, 55, 40],
+                fill: true,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                pointBackgroundColor: 'rgb(255, 99, 132)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(255, 99, 132)'
+            }, {
+                label: 'My Second Dataset',
+                data: [28, 48, 40, 19, 96, 27, 100],
+                fill: true,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgb(54, 162, 235)',
+                pointBackgroundColor: 'rgb(54, 162, 235)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(54, 162, 235)'
+            }]
+
         };
 
         const options = {
@@ -72,7 +83,7 @@ export default class Chart_7 extends Component {
                 </h3>
                 <br/>
                 <br/>
-                <Line data={data} options={options} />
+                <Radar data={data} options={options} />
                 <br/>
                 <br/>
                 <h3 id={styles._H3}
