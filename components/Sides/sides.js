d54cfc7c018fa51550325e749bcd0529fac63d8b
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-27 08:45:00 +0400

    over qualified

--- components/Sides/sides.js
+++ components/Sides/sides.js
@@ -91,34 +91,34 @@ export default class Sides extends Component {
                     <div id={styles.RIGHT_FIXED_STICKY}>
                         <div id={styles.PFBD_LINKS}>
                             <div id={styles.ICON_PLACEHOLDER} class={styles.container}>
-                                <h3 class="drop-shadow-xl ... font-avant_garde_bold" id={styles.SIDE_TEXT} >Welcome     to    <a><Link class="text-emerald-200" href="/">bimcopilot.com</Link></a> </h3>
+                                <h3 class="drop-shadow-xl ... font-avant_garde_bold" id={styles.SIDE_TEXT} >Welcome    <br/> to <br/>   <a><Link class="text-emerald-200" href="/">bimcopilot.com</Link></a> </h3>
                             </div>
                         </div>
     
                         <div id={styles.PAGE_NAVIGATION}>
-                            <div class="bg-stone-200  ..." id={styles.LINE_1}>
+                            <div class="bg-slate-50  ..." id={styles.LINE_1}>
                             </div>
 
                             <div id={styles.NAV_LINK}>
                                
-                                <Link class="bg-stone-200  ..." id={styles.ANCHOR_1} href= "/">
+                                <Link class="bg-slate-50  ..." id={styles.ANCHOR_1} href= "/">
                                 </Link>
 
-                                <Link class="bg-stone-200  ..." id={styles.ANCHOR_2} href= "/">
+                                <Link class="bg-slate-50  ..." id={styles.ANCHOR_2} href= "/">
                                 </Link>
 
-                                <Link class="bg-stone-200  ..." id={styles.ANCHOR_3} href= "/">
+                                <Link class="bg-slate-50 ..." id={styles.ANCHOR_3} href= "/">
                                 </Link>
 
-                                <Link class="bg-stone-200 ..."  id={styles.ANCHOR_4} href= "/">
+                                <Link class="bg-slate-50 ..."  id={styles.ANCHOR_4} href= "/">
                                 </Link>
                                                               
-                                <Link class="bg-stone-200 ..."  id={styles.ANCHOR_5} href= "/">
+                                <Link class="bg-slate-50 ..."  id={styles.ANCHOR_5} href= "/">
                                 </Link>
                                                                 
                             </div>
 
-                            <div class="bg-stone-200 ..." id={styles.LINE_2}>
+                            <div class="bg-slate-50 ..." id={styles.LINE_2}>
                             </div>
                         </div>
                     </div>
