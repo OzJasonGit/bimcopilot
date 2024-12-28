c3a038b6e65bcf291763d7b6ccd42318204c9e70
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-28 16:59:15 +0400

    we're doing it

--- Modules/Copilot_Dashboard/Charts/Chart_7/Chart_7.js
+++ Modules/Copilot_Dashboard/Charts/Chart_7/Chart_7.js
@@ -51,6 +51,9 @@ export default class Chart_7 extends Component {
                 pointHoverBorderColor: 'rgb(54, 162, 235)'
             }]
 
+
+      
+
         };
 
         const options = {
@@ -66,9 +69,15 @@ export default class Chart_7 extends Component {
             scales: {
                 x: {
                     beginAtZero: true, // X-axis starts at zero
+                    grid: {
+                        color: 'rgba(255, 99, 132, 0.2)' // Change X-axis grid color
+                        }
                 },
                 y: {
                     beginAtZero: true, // Y-axis starts at zero
+                    grid: {
+                        color: 'rgba(255, 99, 132, 0.2)' // Change X-axis grid color
+                        }
                 },
             },
         };
