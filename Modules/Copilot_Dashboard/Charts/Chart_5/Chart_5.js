47481d7733f8fd7bbda1c7e57d6408007e7adf5e
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2024-12-29 13:27:02 +0400

    please

--- Modules/Copilot_Dashboard/Charts/Chart_5/Chart_5.js
+++ Modules/Copilot_Dashboard/Charts/Chart_5/Chart_5.js
@@ -57,7 +57,7 @@ export default class Chart_3 extends Component {
         };
 
         return (
-            <div style={{ width: "100%", height: "600px", padding:"0px", justifyItems:"right" }}>           
+            <div style={{ width: "100%", height: "600px", padding:"0px", justifyItems:"left" }}>           
                 <Bar data={data} options={options} style={{ width:"100%", height:'100%', position:'relative',}}/>               
             </div>
         );
