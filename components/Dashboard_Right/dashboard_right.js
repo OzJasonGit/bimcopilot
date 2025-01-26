15bb27ed2a2c146bb509a71d9d160f34767d97aa
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-01-26 11:39:25 +0400

    bimcopilot

--- components/Dashboard_Right/dashboard_right.js
+++ components/Dashboard_Right/dashboard_right.js
@@ -21,17 +21,21 @@ return (
     <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
           <div class={styles.grid_0_bimcopilot}>
             <div id={styles.BIMCOPILOT_CONTAINER}>
-              <div id={styles.BIMCOPILOT}>
 
-                <Image id={styles.CENTER}
-                    src={bimcopilot}   
-                    style={{objectFit: "contain"}} 
-                    quality={100}
-                />  
-                
+
+              <div id={styles.BIMCOPILOT} style={{
+                        position: "relative",                                
+                        height: "100%",
+                        width: "100%",
+                        gridArea: "BIMCOPILOT"}}>
+
+                            <Image id={styles.CENTER}
+                              src={bimcopilot}   
+                              style={{objectFit: "contain"}} 
+                              quality={100}
+                          />  
               </div>
               
-
             </div>
           </div>
         </section>
