2037416f6bc5bcedc478dcc5117ab4f861085413
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-02-01 22:39:40 +0400

    suckeer

--- components/Sign_In/page.js
+++ components/Sign_In/page.js
@@ -86,14 +86,13 @@ const SignIn = () => {
           <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
           <ToastContainer />
           <div
-            className="min-h-screen flex items-center justify-center"
             style={{
               gridArea: "SUB",             
             }}
           >
             <div
               className={`max-w-md min-w-[320px] min-h-[605px] p-6 bg-gray-300 rounded-md shadow-md  ${
-                isLoading && "flex justify-left items-left"
+                isLoading && "flex justify-left items-center"
               }`}
             >
               {isLoading ? (
