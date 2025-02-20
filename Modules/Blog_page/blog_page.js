1e7f5bbef1cbb89ca09b8807ed5c6921a3c0a189
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-02-20 21:38:29 +0400

    water

--- Modules/Blog_page/blog_page.js
+++ Modules/Blog_page/blog_page.js
@@ -386,7 +386,7 @@ const Blog_page = (stories) => {
                                 style={{
                                     position: "relative",
                                     width: "100%",
-                                    height: "300px",
+                                    height: "auto",
                                     gridArea: "VIDEO",
                                 }}>
                                 <Video />
