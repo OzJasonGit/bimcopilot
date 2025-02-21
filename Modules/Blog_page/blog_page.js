6a788b8a1c81aacd21d7ebb49901217eebffaf81
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-02-21 23:47:11 +0400

    common

--- Modules/Blog_page/blog_page.js
+++ Modules/Blog_page/blog_page.js
@@ -105,7 +105,7 @@ const Blog_page = (stories) => {
 
                             <div id="SUB_TAG">
                                 <div
-                                    /*id="_H3"*/
+                                    id="_H3"
                                     className="text-left ... text-xl ... text-stone-400 ... font-avant_garde_medium">                                     
                                     {parse(story.subtitle || "")}         
                                 </div>
