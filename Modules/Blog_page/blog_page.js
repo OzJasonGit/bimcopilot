8a8ced6e9f4ec0f8eb76fcc437efffa9c08b0dbf
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-02-22 17:42:20 +0400

    tesla

--- Modules/Blog_page/blog_page.js
+++ Modules/Blog_page/blog_page.js
@@ -189,7 +189,7 @@ const Blog_page = (stories) => {
 
 
 
-    <section id="SHADOW_SECTION_TITLE" className="center_holder">
+    <section id="SHADOW_SECTION_SIDE" className="center_holder">
 
         <div className="grid_0_blog">
 
