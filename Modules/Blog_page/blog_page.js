f250e6a1428e44227a9fe29e987dc871f86de706
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-02-22 15:52:17 +0400

    PCOS

--- Modules/Blog_page/blog_page.js
+++ Modules/Blog_page/blog_page.js
@@ -1005,7 +1005,11 @@ const Blog_page = (stories) => {
                     </h1>
                     <p  
                         className="text-lg ... text-neutral-700 ... font-avant_garde_medium">
-                        {parse(story.Conclusion || "")}                  
+                        {parse(story.Conclusion || "")}   
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                        fugiat nulla pariatur.                
                     </p>                  
                 </div>
 
