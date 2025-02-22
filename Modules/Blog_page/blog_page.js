74579345d391f3ffa8823ce12f4ece0abc4e6121
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-02-22 15:56:57 +0400

    cook

--- Modules/Blog_page/blog_page.js
+++ Modules/Blog_page/blog_page.js
@@ -496,7 +496,7 @@ const Blog_page = (stories) => {
                     style={{
                         position: "relative",
                         width: "100%",
-                        height: "320px",
+                        height: "350px",
                         gridArea: "IMAGE",
                     }}>
                         <Image
