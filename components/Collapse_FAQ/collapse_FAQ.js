25e2a33fa0621424b9c60c9ebf44aeac3dd4ab80
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-03-15 22:30:10 +0400

    colleague

--- components/Collapse_FAQ/collapse_FAQ.js
+++ components/Collapse_FAQ/collapse_FAQ.js
@@ -67,7 +67,8 @@ const items = [
     key: '2',
     label: <h3 style={{ color: "rgb(68 64 60)", alignItems: "end" }}
       class="font-avant_garde_bold"
-      id={styles._H3}> How can your AI-driven tools help my architecture, design or manufacturing business?
+      id={styles._H3}> How can your AI-driven tools help my architecture, design
+      <br/> or manufacturing business?
     </h3>,
     children: <p style={{ position: "relative", left: "25px" }} > {text_2} </p>
   },
@@ -76,7 +77,8 @@ const items = [
     key: '3',
     label: <h3 style={{ color: "rgb(68 64 60)", alignItems: "end" }}
       class="font-avant_garde_bold"
-      id={styles._H3}> Do you offer custom design solutions, or are your products pre-made?
+      id={styles._H3}> Do you offer custom design solutions, or are your 
+      <br/> products pre-made?
     </h3>,
     children: <p style={{ position: "relative", left: "25px" }} > {text_3} </p>
   },
@@ -85,7 +87,8 @@ const items = [
     key: '4',
     label: <h3 style={{ color: "rgb(68 64 60)", alignItems: "end" }}
       class="font-avant_garde_bold"
-      id={styles._H3}>Are your digital products easy to integrate into my existing workflow?
+      id={styles._H3}>Are your digital products easy to integrate into 
+      <br/> my existing workflow?
     </h3>,
     children: <p style={{ position: "relative", left: "25px" }} > {text_4} </p>
   },
@@ -94,7 +97,8 @@ const items = [
     key: '5',
     label: <h3 style={{ color: "rgb(68 64 60)", alignItems: "end" }}
       class="font-avant_garde_bold"
-      id={styles._H3}> Can your automation tools help me save time on repetitive design tasks?
+      id={styles._H3}> Can your automation tools help me save time on 
+      <br/> repetitive design tasks?
     </h3>,
     children: <p style={{ position: "relative", left: "25px" }} > {text_5} </p>
   },
@@ -103,7 +107,8 @@ const items = [
     key: '6',
     label: <h3 style={{ color: "rgb(68 64 60)", alignItems: "end" }}
       class="font-avant_garde_bold"
-      id={styles._H3}>What makes your services different from other design and tech solutions?
+      id={styles._H3}>What makes your services different from other design 
+      <br/> and tech solutions?
     </h3>,
     children: <p style={{ position: "relative", left: "25px" }} > {text_6} </p>
   },
@@ -112,7 +117,8 @@ const items = [
     key: '7',
     label: <h3 style={{ color: "rgb(68 64 60)", alignItems: "end" }}
       class="font-avant_garde_bold"
-      id={styles._H3}>Do you work with individual professionals, small firms, or large businesses?
+      id={styles._H3}>Do you work with individual professionals, small firms, or 
+      <br/> large businesses?
     </h3>,
     children: <p style={{ position: "relative", left: "25px" }} > {text_7} </p>
   },
@@ -129,7 +135,8 @@ const items = [
    {  key: '9',
     label: <h3 style={{ color: "rgb(68 64 60)", alignItems: "end", paddingBottom: "0px" }}
       class="font-avant_garde_bold"
-      id={styles._H3}> How do I get started with your services, and what are the pricing options?
+      id={styles._H3}> How do I get started with your services, and what are 
+      <br/> the pricing options?
     </h3>,
     children: <p style={{ position: "relative", left: "25px" }}  > {text_9} </p>
   },
