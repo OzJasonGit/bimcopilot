e987d43d3a6fa95d5b1625bad20a06ca558e5180
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-04-20 21:59:37 +0400

    affair

--- components/Collapse_Services/collapse_Services.js
+++ components/Collapse_Services/collapse_Services.js
@@ -49,7 +49,7 @@ const items = [
           <li class="font-geist_regular ... text-stone-400 ..."> 
               <a class="font-geist_semibold ... text-stone-200 ...">Design Automation Tools - 
               </a> 
-                Bespoke Revit templates and families - 
+                 Bespoke Revit templates and families - 
                 <br/> Streamlined documentation workflows - 
                 Cloud coordination - 
                 Bespoke Dynamo scripting. 
@@ -58,7 +58,7 @@ const items = [
           <li class="font-geist_regular ... text-stone-400 ..."> 
               <a class="font-geist_semibold ... text-stone-200 ...">Advanced Parametric Design – 
               </a> 
-                Grasshopper facade and envelope Scripts - 
+                 Grasshopper facade and envelope Scripts - 
                 Parametric design solutions -  
                 Advanced parametric form finding.   
           </li>
@@ -66,7 +66,7 @@ const items = [
           <li class="font-geist_regular ... text-stone-400 ..."> 
               <a class="font-geist_semibold ... text-stone-200 ...">Project and Design Management – 
               </a> 
-                End-to-end project coordination (Coordination between clients,
+                 End-to-end project coordination (Coordination between clients,
                 <br/> consultants, and contractors) - 
                 QA/QC checklists tailored to your project -  
                 Resolving conflicts and aligning goals early -  
