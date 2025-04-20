990e2997079695b2a62c2add4abc5ef89a364f25
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-04-20 21:33:20 +0400

    dollars

--- components/Collapse_Services/collapse_Services.js
+++ components/Collapse_Services/collapse_Services.js
@@ -42,7 +42,7 @@ const items = [
 
       {text_1} 
       <br/>    
-      <ul class="list-disc ... font-geist_regular ... text-stone-200 ..."
+      <ul class="list-disc ... font-geist_regular ... text-stone-400 ..."
           id={styles._H3_2}
           style={{ position: "relative", left: "35px" }}>
           <li> <a class="font-geist_semibold ... text-stone-200 ...">Design Automation Tools - </a> 
