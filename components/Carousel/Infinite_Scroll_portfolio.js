4ed8acbb4fd01a295a3e035bdf62a2f6d7b866a8
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-05-16 21:40:25 +0400

    threaten

--- components/Carousel/Infinite_Scroll_portfolio.js
+++ components/Carousel/Infinite_Scroll_portfolio.js
@@ -21,8 +21,8 @@ const TAGS = [
                 <div className=""                    
                      style={{
                      padding: "0px",
-                     width: "800px",
-                     height: "1000px"
+                     width: "1000px",
+                     height: "1200px"
                      }}>
                       <div 
                            id={styles.CAROUSEL_GRID_1}
