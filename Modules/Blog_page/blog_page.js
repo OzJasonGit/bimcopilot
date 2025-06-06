9eba700bca0c222037588bbec5e70dd464cf3448
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-06-06 11:55:45 +0400

    sub_image_height

--- Modules/Blog_page/blog_page.js
+++ Modules/Blog_page/blog_page.js
@@ -763,7 +763,7 @@ const Blog_page = (stories) => {
                              style={{
                                 position: "relative",
                                 width: "100%",
-                                height: "350px",
+                              
                                 gridArea: "IMAGE",
                              }}>
                             <Image
