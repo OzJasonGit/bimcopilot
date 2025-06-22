c8725b22199d1a32d9ea318d1ccf45e244b75196
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-06-22 07:05:30 +0400

    seduction

--- Modules/Blog_page/blog_page.js
+++ Modules/Blog_page/blog_page.js
@@ -400,7 +400,7 @@ const Blog_page = (stories) => {
                                                                             style={{
                                                                             position: "relative",                                                  
                                                                             width: "100%",
-                                                                            height: "300px",
+                                                                            height: "400px",
                                                                             gridArea: "PFBD"
                                                                             }}>
                                                                             <div class={styles.PARAGRAPH_10}
