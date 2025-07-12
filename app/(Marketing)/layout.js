709bd09bc80dd58eb3b39ce80212fff7ab945470
moon <moon@moons-MacBook-Pro.local>
2025-07-12 22:25:56 +0500

    links updated

--- app/(Marketing)/layout.js
+++ app/(Marketing)/layout.js
@@ -1,7 +1,7 @@
 // app/(Marketing)/layout.js
 "use client";
 
-import { CartProvider } from '@/components/context/CartContext';
+import { CartProvider } from '../../components/context/CartContext';
 import { Toaster } from 'sonner';
 
 export default function MarketingLayout({ children }) {
