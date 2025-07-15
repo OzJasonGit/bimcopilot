76afdbc309858fae2145db550f8a8ebae913e5db
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-07-15 21:33:11 +0400

    apple

--- Modules/Blog_page/blog_page.js
+++ Modules/Blog_page/blog_page.js
@@ -348,27 +348,27 @@ const Blog_page = (stories) => {
                                                                                         class="text-left ... text-neutral-700 ... font-avant_garde_medium"
                                                                                     >
                                                                                         {" "}
-                                                                                        <a class="text-neutral-700 ... font-avant_garde_bold">
+                                                                                        <a class="text-stone-200 ... font-avant_garde_bold">
                                                                                         The design revolution is here.
                                                                                         </a>{" "}
                                                                                         The world is changing and so is{" "}
-                                                                                        <a class="text-neutral-700 ... font-avant_garde_bold">
+                                                                                        <a class="text-stone-200 ... font-avant_garde_bold">
                                                                                         architecture
                                                                                         </a>
                                                                                         . Discover new narratives, build better{" "}
-                                                                                        <a class="text-neutral-700 ... font-avant_garde_bold">
+                                                                                        <a class="text-stone-200 ... font-avant_garde_bold">
                                                                                         systems
                                                                                         </a>
                                                                                         , make more{" "}
-                                                                                        <a class="text-neutral-700 ... font-avant_garde_bold">money</a>,
+                                                                                        <a class="text-stone-200 ... font-avant_garde_bold">money</a>,
                                                                                         be more{" "}
-                                                                                        <a class="text-neutral-700 ... font-avant_garde_bold">
+                                                                                        <a class="text-stone-200 ... font-avant_garde_bold">
                                                                                         sustainable
                                                                                         </a>
                                                                                         . <br /> <br />{" "}
                                                                                         <h1 
                                                                                         id={styles._H1} 
-                                                                                        class="text-neutral-700 ... font-avant_garde_bold">
+                                                                                        class="text-stone-200 ... font-avant_garde_bold">
                                                                                         Join the Waiting List!
                                                                                         </h1>
                                                                                     </h3>
