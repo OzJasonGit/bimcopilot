939b10ad98ca8a5808de2867ddb18ad743049f39
moon <moon@moons-MacBook-Pro.local>
2025-07-28 23:16:13 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,6 @@ const nextConfig = {
       contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
     experimental: {
-      optimizeCss: true,
       optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
     },
     compiler: {
