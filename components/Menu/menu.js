5d69d1be5acaf1bf328cd1157275bcdee4fcca44
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-08-02 18:56:59 +0400

    tension

--- components/Menu/menu.js
+++ components/Menu/menu.js
@@ -263,17 +263,24 @@ const Menu = () => {
 
 
                                   <a id={styles.FAVOURITES} className="content-center ... align-middle ..." href="/cart">
-                                  <div className="content-center ... align-middle ..." >
-                                      <FontAwesomeIcon icon={faHeart} size="3xs" className="text-slate-50" />
+                                  <div className="content-center ... align-middle ..."
+                                        style={{position:'relative',
+                                                left:'-70px',
+                                                top:'0.5px',
+                                               }}>
+                                      <FontAwesomeIcon icon={faHeart} size="xl" className="text-stone-50" />
                                     </div>
                                   </a>
 
                                   <a id={styles.CART} className="content-center ... align-middle ..." href="/cart">
                                     <div className="content-center ... align-middle ..."
                                       style={{
-                                        paddingLeft: "30px",                                      
+                                        paddingLeft: "32px", 
+                                        position:'relative',
+                                                left:'-70px',
+                                                top:'0.5px',                                     
                                       }}>
-                                      <FontAwesomeIcon icon={faCartShopping} size="3xs" className="text-slate-50" />
+                                      <FontAwesomeIcon icon={faCartShopping} size="xl" className="text-stone-50" />
                                     </div>
                                   </a>
 
