c582d105b338230e664c61db8a89072669542a86
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-09-07 01:03:38 +0400

    hunza

--- components/Menu_White/Burger/burger.js
+++ components/Menu_White/Burger/burger.js
@@ -4,43 +4,29 @@ import styles from "./burger.module.css";
 
 
 
-export default function BurgerMenu() {
-  const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <button
-      onClick={() => setIsOpen(!isOpen)}
-      className="relative w-8 h-8 flex flex-col justify-between items-center cursor-pointer group"
-    >
-      {/* Top line */}
-      <span
-        className={`block h-1 w-8 bg-stone-200 rounded transition-all duration-300 ${
-          isOpen ? "rotate-45 translate-y-3" : ""
-        }`}
-      ></span>
 
-      {/* Middle line */}
-      <span
-        className={`block h-1 w-8 bg-stone-200 rounded transition-all duration-300 ${
-          isOpen ? "opacity-0" : ""
-        }`}
-      ></span>
+const Hamburger = () => {
+  const [isActive, setIsActive] = useState(false);
 
-      {/* Bottom line */}
-      <span
-        className={`block h-1 w-8 bg-stone-200 rounded transition-all duration-300 ${
-          isOpen ? "-rotate-45 -translate-y-3" : ""
-        }`}
-      ></span>
-    </button>
-  );
+  const toggleClass = () => {
+    setIsActive(!isActive);
+  };
+
+    return (
+        <>
+            <div
+                className={`hamburger ${isActive ? "is-active" : ""}`}
+                onClick={toggleClass}>
+
+                <div class={styles.hamburger} id={styles.hamburger-11}>
+                    <span class={styles.line}></span>
+                    <span class={styles.line}></span>
+                    <span class={styles.line}></span>
+                </div>  
+            </div>           
+        </>     
+    );
 }
 
-
-
-
-
-
-
-
-
+export default Hamburger;
