e811dd40641fe33c42db0ccfdcfb30efd7701591
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-09-07 14:24:15 +0400

    sides

--- Modules/Cart/Cart.js
+++ Modules/Cart/Cart.js
@@ -2,11 +2,13 @@
 
 
 import Cart from "@/components/Cart/Cart";
+import Sides from "../../components/Sides/sides";
 
 
 const CartModule = () => {
     return (
       <>
+        <Sides/>
         <Cart/>
 
       </>
--- components/Basket/page.js
+++ components/Basket/page.js
@@ -14,7 +14,7 @@ import { Button } from "@/components/ui/button"
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Menu from "../Menu/menu";
-import Sides from "../../components/Sides/sides";
+
 
 
 
@@ -83,7 +83,7 @@ const Basket = () => {
     <>
       <Menu/>
       <Header/>
-      <Sides/>
+      
 
       <section id={styles.SHADOW_SECTION_BLOG} class={styles.center_holder}>
         <div class={styles.grid_0_blogimageholder}>
