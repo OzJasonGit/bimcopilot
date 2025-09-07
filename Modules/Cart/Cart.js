dfa82f0c07b5d4c13890c8d065056237184f1334
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-09-07 15:05:41 +0400

    war

--- Modules/Cart/Cart.js
+++ Modules/Cart/Cart.js
@@ -4,20 +4,12 @@
 import Cart from "@/components/Cart/Cart";
 import Sides from "../../components/Sides/sides";
 
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Menu from "../Menu/menu";
-
 
 const CartModule = () => {
     return (
       <>
-        <Menu/>
-        <Header/>
-        <Sides/>
+      <Sides/>
         <Cart/>
-
-        <Footer/>
 
       </>
     );
