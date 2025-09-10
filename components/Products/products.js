4dcd4af085e13875d31724e40084a5fd4bc840f8
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-09-10 21:00:41 +0400

    chineese

--- components/Products/products.js
+++ components/Products/products.js
@@ -141,7 +141,7 @@ export default class Products extends Component {
 
 
                     {/* Dark overlay */}
-                    <div className="absolute inset-0 bg-black/90 opacity-0 transition-opacity duration-500 group-hover:opacity-100 z-20" />
+                    <div className="absolute inset-0 bg-black/80 opacity-0 transition-opacity duration-500 group-hover:opacity-100 z-20" />
 
 
                     {/* Icons */}
