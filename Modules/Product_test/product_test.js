16d54ec85ff4837dcb131c4ea58dea5659dfd859
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-09-13 12:23:35 +0400

    south africans

--- Modules/Product_test/product_test.js
+++ Modules/Product_test/product_test.js
@@ -93,7 +93,7 @@ const Products = () => {
                               className="group overflow-hidden rounded-2xl cursor-pointer transition-all duration-500 shadow-none hover:shadow-xl"
                               style={{ position: "relative", width: "100%", height: "100%" }}>
 
-                          <div id={styles.imageWrapper} className="relative w-full h-full transition-transform duration-500 ease-in-out transform-gpu origin-center scale-110 group-hover:scale-100 ">
+                          <div id={styles.imageWrapper}>
                             <Image
                               src={product.image}
                               alt={product.title}
@@ -141,7 +141,7 @@ const Products = () => {
                                     className="group overflow-hidden rounded-2xl cursor-pointer transition-all duration-500 shadow-none hover:shadow-xl"
                                     style={{ position: "relative", width: "100%", height: "auto" }}>
 
-                                <div id={styles.imageWrapper} className="transition-transform duration-500 ease-in-out transform-gpu origin-center scale-110 group-hover:scale-100 ">
+                                <div id={styles.imageWrapper}>
                                   <Image
                                     src={group[3].image}
                                     alt={group[3].title}
@@ -179,7 +179,7 @@ const Products = () => {
                                     className="group overflow-hidden rounded-2xl cursor-pointer transition-all duration-500 shadow-none hover:shadow-xl"
                                     style={{ position: "relative", width: "100%", height: "auto" }}>
 
-                                <div id={styles.imageWrapper} className="transition-transform duration-500 ease-in-out transform-gpu origin-center scale-110 group-hover:scale-100 ">
+                                <div id={styles.imageWrapper}>
                                   <Image
                                     src={group[4].image}
                                     alt={group[4].title}
@@ -239,7 +239,7 @@ const Products = () => {
                                     className="group overflow-hidden rounded-2xl cursor-pointer transition-all duration-500 shadow-none hover:shadow-xl"
                                     style={{ position: "relative", width: "100%", height: "auto" }}>
 
-                                <div id={styles.imageWrapper} className="relative w-full h-full transition-transform duration-500 ease-in-out transform-gpu origin-center scale-110 group-hover:scale-100 ">
+                                <div id={styles.imageWrapper}>
                                   <Image
                                     src={group[4].image}
                                     alt={group[4].title}
@@ -315,6 +315,8 @@ const Products = () => {
                                 <p className={styles.subtitle} class=" text-stone-200 ... font-avant_garde_medium ...">{parse(group[3].subtitle)}</p>
                               </div>
                             </div>
+
+
                           </>
                         )}
                       </>
