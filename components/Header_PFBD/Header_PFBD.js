7186145fd0da9ea617fdf3d3ffd72ecaa7ec3ecb
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-09-23 20:14:53 +0400

    status

--- components/Header_PFBD/Header_PFBD.js
+++ components/Header_PFBD/Header_PFBD.js
@@ -87,7 +87,7 @@ export default function Header() {
     <>
       <section id={styles.SHADOW_SECTION} className={styles.center_holder}>
         <div className={styles.HEADER_HOLDER}>
-          <div id={styles.header}>
+          <div id={styles.HEADER}>
             <header id={styles.FIXED_HEADER}>
               {/* Logo */}
               <div id={styles.LOGO_ICON} className={styles.container}>
