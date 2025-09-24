afb8fdb55df6e163c93957a3a283bdf1327ad4db
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-09-24 19:11:15 +0400

    commited

--- Modules/Plastic_Free_By_Design/plastic_freee_by_design.js
+++ Modules/Plastic_Free_By_Design/plastic_freee_by_design.js
@@ -107,7 +107,7 @@ const PFBD = () => {
               
                   zIndex: '1'
                 }}>
-                  <h1 id={styles._H1_LARGE} className='text-stone-50 font-margin_demo'>This is...<br/>Plastic Free_<br/>By Design</h1>
+                  <h1 id={styles._H1_LARGE} className='text-stone-50 font-margin_demo'><a id={styles._H2} >This is...</a><br/>Plastic Free_<br/>By Design</h1>
             </div>
 
             <div  id={styles.SUBTITLE}
