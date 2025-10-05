b7f22a558a22c0e282e4fff2db715c1deefa66e2
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-10-05 10:45:41 +0400

    known

--- components/Menu/menu.js
+++ components/Menu/menu.js
@@ -91,8 +91,6 @@ const Menu = () => {
                   <h3 id={styles._H2} class="text-right ...  text-stone-700  font-avant_garde_bold"><a class="">Shop</a></h3>
                 </Link>  
 
-
-
                 <div id={styles.DRAWER_ICONS}>
                   <div id={styles.PFBD_ICON} class={styles.icon}>
                       <Link id={styles.PFBD_LINK} href= "/" target="_blank">
@@ -134,103 +132,97 @@ const Menu = () => {
                           </div> 
                       </Link>
                   </div>
+                </div>        
+
+                <div id={styles.SOCIAL}>
+
+                    <div id={styles.ARB_ICONS}>
+
+                        <div class=" rounded-md ..." id={styles.ARB}>
+                            <Link href= "/">
+                                <Image
+                                    src={logoARB}
+                                    alt="Picture of the author"
+                                    width={500}
+                                    height={500}
+                                    loading="lazy"  
+                                    style={{
+                                        position: "relative",
+                                        width: "100%",
+                                        height: "100%",
+                                        objectFit: "cover",
+                                    }}
+                                /> 
+                            </Link>
+                        </div>
+
+
+                        <div class=" rounded-md ..." id={styles.RIBA}>
+                            <Link href= "/">
+                                <Image
+                                    src={logoRIBA}
+                                    alt="Picture of the author"
+                                    width={500}
+                                    height={500}
+                                    loading="lazy"  
+                                    style={{
+                                        position: "relative",
+                                        width: "100%",
+                                        height: "100%",
+                                        objectFit: "cover",
+                                    }}
+                                /> 
+                            </Link>
+                        </div>  
+
+                    </div>
+
+                    <div id={styles.MEDIUM_ICON} class={styles.icon}>
+                        <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
+                                  <FontAwesomeIcon class={styles.medium} id={styles.CENTER} icon={faMedium} size="2xs" />
+                        </a>
+                    </div>  
+                    <div id={styles.LINKEDIN_ICON} class={styles.icon}>
+                        <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
+                                  <FontAwesomeIcon class={styles.linkedin} id={styles.CENTER} icon={faLinkedin} size="2xs" />
+                        </a>
+                    </div>   
+                    <div id={styles.YOUTUBE_ICON} class={styles.icon}>
+                        <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
+                                  <FontAwesomeIcon class={styles.youtube} id={styles.CENTER} icon={faYoutube} size="2xs" />
+                        </a>
+                    </div>   
+                    <div id={styles.DRIBBLE_ICON} class={styles.icon}>
+                        <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
+                                  <FontAwesomeIcon class={styles.dribbble} id={styles.CENTER} icon={faDribbble} size="2xs" />
+                        </a>
+                    </div>   
+                    <div id={styles.GITHUB_ICON} class={styles.icon}>
+                        <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
+                                  <FontAwesomeIcon class={styles.github} id={styles.CENTER} icon={faGithub} size="2xs" />
+                        </a>
+                    </div>                     
                 </div>
 
-
-
-
-
-             
-
-                 <div id={styles.SOCIAL}>
-
-                  <div id={styles.ARB_ICONS}>
-
-                      <div class=" rounded-md ..." id={styles.ARB}>
-                          <Link href= "/">
-                              <Image
-                                  src={logoARB}
-                                  alt="Picture of the author"
-                                  width={500}
-                                  height={500}
-                                  loading="lazy"  
-                                  style={{
-                                      position: "relative",
-                                      width: "100%",
-                                      height: "100%",
-                                      objectFit: "cover",
-                                  }}
-                              /> 
-                          </Link>
-                      </div>
-
-
-                      <div class=" rounded-md ..." id={styles.RIBA}>
-                          <Link href= "/">
-                              <Image
-                                  src={logoRIBA}
-                                  alt="Picture of the author"
-                                  width={500}
-                                  height={500}
-                                  loading="lazy"  
-                                  style={{
-                                      position: "relative",
-                                      width: "100%",
-                                      height: "100%",
-                                      objectFit: "cover",
-                                  }}
-                              /> 
-                          </Link>
-                      </div>  
-
-                  </div>
-
-                  <div id={styles.MEDIUM_ICON} class={styles.icon}>
-                      <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
-                                <FontAwesomeIcon class={styles.medium} id={styles.CENTER} icon={faMedium} size="2xs" />
-                      </a>
-                  </div>  
-                  <div id={styles.LINKEDIN_ICON} class={styles.icon}>
-                      <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
-                                <FontAwesomeIcon class={styles.linkedin} id={styles.CENTER} icon={faLinkedin} size="2xs" />
-                      </a>
-                  </div>   
-                  <div id={styles.YOUTUBE_ICON} class={styles.icon}>
-                      <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
-                                <FontAwesomeIcon class={styles.youtube} id={styles.CENTER} icon={faYoutube} size="2xs" />
-                      </a>
-                  </div>   
-                  <div id={styles.DRIBBLE_ICON} class={styles.icon}>
-                      <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
-                                <FontAwesomeIcon class={styles.dribbble} id={styles.CENTER} icon={faDribbble} size="2xs" />
-                      </a>
-                  </div>   
-                  <div id={styles.GITHUB_ICON} class={styles.icon}>
-                      <a id={styles.PFBD_LINK} href="https://github.com/OzJasonGit" target="_blank">
-                                <FontAwesomeIcon class={styles.github} id={styles.CENTER} icon={faGithub} size="2xs" />
-                      </a>
-                  </div>                     
-              </div>
-
                 <div id={styles.TEXT}>
-                  <div id={styles.COMPANY}>
-                      <h4 id={styles._H4}  class=" text-xs ... text-stone-700 ... font-avant_garde_bold text-left">Website by <a class="text-stone-400 font-avant_garde_bold">
-                      <Link  href="/">designlab.pfbd</Link> 
-                      </a>. Made with love, with a gun to our head.
-                      </h4>
-                      <br/>
-                      <h4 id={styles._H4}  class="text-stone-700 ... font-avant_garde_bold text-xs ... text-left"><a class= "text-emerald-400 ... font-avant_garde_bold"><Link href="/">bimcopilot.com</Link></a> 
-                          _ a subsidiary of <a class= "font-avant_garde_bold  text-stone-400 ...">Plastic Free By Design Ltd</a>
-                      </h4>
-                      <br/>
-                      <h4 id={styles._H4}  class="text-stone-700 ... font-avant_garde_bold text-xs ... text-  left">
-                          <a class= "font-avant_garde_bold  text-stone-400 ..."> 
-                              Plastic Free By Design Ltd          
-                          </a>  A limited liability company registered in England and Wales
-                          <br></br>
-                          Company No. 15015616 
-                      </h4>
-                  </div>
+                    <div id={styles.COMPANY}>
+                        <h4 id={styles._H4}  class=" text-xs ... text-stone-700 ... font-avant_garde_bold text-left">Website by <a class="text-stone-400 font-avant_garde_bold">
+                        <Link  href="/">designlab.pfbd</Link> 
+                        </a>. Made with love, with a gun to our head.
+                        </h4>
+                        <br/>
+                        <h4 id={styles._H4}  class="text-stone-700 ... font-avant_garde_bold text-xs ... text-left"><a class= "text-emerald-400 ... font-avant_garde_bold"><Link href="/">bimcopilot.com</Link></a> 
+                            _ a subsidiary of <a class= "font-avant_garde_bold  text-stone-400 ...">Plastic Free By Design Ltd</a>
+                        </h4>
+                        <br/>
+                        <h4 id={styles._H4}  class="text-stone-700 ... font-avant_garde_bold text-xs ... text-  left">
+                            <a class= "font-avant_garde_bold  text-stone-400 ..."> 
+                                Plastic Free By Design Ltd          
+                            </a>  A limited liability company registered in England and Wales
+                            <br></br>
+                            Company No. 15015616 
+                        </h4>
+                    </div>
                 </div>
                 
               </div>
@@ -241,55 +233,8 @@ const Menu = () => {
 
         
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
           <div class={styles.HEADER_HOLDER}>
-             <div id={styles.header}>   
+             <div id={styles.HEADER}>   
                 
                 <header id={styles.FIXED_HEADER}> 
                     <div id={styles.BUTTON_HOLDER_RIGHT} class={styles.container}>
