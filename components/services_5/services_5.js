ff73364002c8a327ee41054e12da46bc635b3b57
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-10-21 21:18:57 +0400

    feminised

--- components/services_5/services_5.js
+++ components/services_5/services_5.js
@@ -15,7 +15,15 @@ import {Divider} from "@heroui/react";
 
 export default class Services_5 extends Component {
 
+
+
 render() {
+
+
+
+ const defaultContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
    
 return (
 
@@ -96,15 +104,15 @@ return (
                         {/*<Divider />*/}
                         <div> 
                           <br/>
-                            <Accordion variant="light">
+                            <Accordion>
                               <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
-                               
+                                {defaultContent}
                               </AccordionItem>
                               <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-                             
+                                {defaultContent}
                               </AccordionItem>
                               <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-                             
+                                {defaultContent}
                               </AccordionItem>
                             </Accordion>
 
