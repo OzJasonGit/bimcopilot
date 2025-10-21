a4c658226e2f7e0b65aebf463a613dfdfad16ba6
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-10-21 21:13:30 +0400

    women

--- components/services_5/services_5.js
+++ components/services_5/services_5.js
@@ -98,13 +98,13 @@ return (
                           <br/>
                             <Accordion variant="light">
                               <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
-                                {defaultContent}
+                               
                               </AccordionItem>
                               <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-                                {defaultContent}
+                             
                               </AccordionItem>
                               <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-                                {defaultContent}
+                             
                               </AccordionItem>
                             </Accordion>
 
