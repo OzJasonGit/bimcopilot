5636834636896ea5c539b6a75204342f23670b80
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2025-11-12 22:38:53 +0400

    click moment

--- components/Products/products.js
+++ components/Products/products.js
@@ -94,12 +94,12 @@ export default class Products extends Component {
                           <Link href={`/products/${product.slug}`}>
                             <h3 id={styles._H3} 
                                 style={{ marginBottom: '4px' }} 
-                                className="text-stone-50 font-avant_garde_bold" >
+                                className="text-stone-700 font-avant_garde_bold" >
                               {product.short_description}
                             </h3>
                             <h3 id={styles._H3} 
                                 style={{ marginTop: '4px' }}
-                                className="text-stone-50 font-avant_garde_medium">
+                                className="text-stone-700 font-avant_garde_medium">
                                 From ${product.commercial_price}
                             </h3>
                           </Link>
