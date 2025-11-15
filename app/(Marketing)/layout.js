4c86e7624a71b1a8ff47bc55d69ed89e73ed3a1a
almightymoon <atharqulimoon@gmail.com>
2025-11-16 01:23:58 +0500

    currency wrapper added

--- app/(Marketing)/layout.js
+++ app/(Marketing)/layout.js
@@ -4,7 +4,6 @@
 
 
 import { CartProvider } from '../../components/Context/CartContext';
-import { CurrencyProvider } from '../../components/Context/CurrencyContext';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 import { Toaster } from 'sonner';
@@ -18,14 +17,12 @@ import { Toaster } from 'sonner';
 
 export default function MarketingLayout({ children }) {
   return (
-    <CurrencyProvider>
-      <PayPalScriptProvider options={{ "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '', currency: 'USD' }}>
-        <CartProvider>
-          <Toaster richColors position="top-right" />
-          {children}
-        </CartProvider>
-      </PayPalScriptProvider>
-    </CurrencyProvider>
+    <PayPalScriptProvider options={{ "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '', currency: 'USD' }}>
+      <CartProvider>
+        <Toaster richColors position="top-right" />
+        {children}
+      </CartProvider>
+    </PayPalScriptProvider>
   );
 }
 
