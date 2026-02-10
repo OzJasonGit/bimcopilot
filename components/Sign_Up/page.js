ef76c76f64adcdb70971096a75ded9cd9906ca9c
almightymoon <almightymoon@users.noreply.github.com>
2026-02-10 18:44:49 +0500

    confirm password masked

--- components/Sign_Up/page.js
+++ components/Sign_Up/page.js
@@ -17,7 +17,7 @@ import Cookies from "js-cookie"; // Import js-cookie for handling cookies
 
 
 
-const Signup = () => {
+const SignupForm = () => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({
@@ -98,6 +98,94 @@ const Signup = () => {
   });
 
   return (
+    <div className={`${isLoading && "flex justify-center items-center"}`}>
+      {isLoading ? (
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : (
+        <>
+          <h2 id={styles._H2} className="text-left text-stone-400 font-avant_garde_bold">Sign Up to Bimcopilot</h2>
+
+          <button
+            type="button"
+            className={styles.googleCustomButton}
+            onClick={() => startGoogleLogin()}
+          >
+            <span className={styles.googleIcon} aria-hidden="true" />
+            <span>Sign in with Google</span>
+          </button>
+
+          <form onSubmit={handleSubmit}>
+            <div id={styles.BREAKLINE}>
+              <div id={styles.LINE_1}
+                style={{
+                  width: "100%",
+                  height: "1px",
+                }}>
+              </div>
+
+              <div id={styles.TEXT}>
+                <h5 className=" text-stone-400 ... ">or</h5>
+              </div>
+
+              <div id={styles.LINE_2}
+                style={{
+                  width: "100%",
+                  height: "1px",
+                }}>
+              </div>
+            </div>
+            <br />
+
+            {["email", "userName", "password", "confirmPassword"].map((field) => (
+              <div key={field} className="mb-4">
+                <label htmlFor={field} className="text-left text-stone-400 font-avant_garde_bold" id={styles._H3}>
+                  {field.replace(/([A-Z])/g, " $1").replace(/^./, (str) => str.toUpperCase())}
+                </label>
+
+                <input
+                  type={field.toLowerCase().includes("password") ? "password" : "text"}
+                  id={field}
+                  name={field}
+                  value={formData[field]}
+                  onChange={handleChange}
+                  className="mt-1 border rounded-md"
+                  required
+                  style={{ width: "100%", height: "40px", padding: "15px" }}
+                />
+              </div>
+            ))}
+
+            <Button variant="outline"
+              id={styles.FORM_BUTTON} type="submit" disabled={isLoading}>
+              Create an Account
+            </Button>
+          </form>
+        </>
+      )}
+    </div>
+  );
+};
+
+const Signup = () => {
+  return (
     <>
       <Menu />
       <Header />
@@ -117,95 +205,7 @@ const Signup = () => {
         <div className={styles.grid_0_blogimageholder}>
           <div id={styles.SIGN_UP}>
             <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
-
-              <div className={`${isLoading && "flex justify-center items-center"}`}>
-                {isLoading ? (
-                  <div role="status">
-                    <svg
-                      aria-hidden="true"
-                      className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                      viewBox="0 0 100 101"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"
-                      />
-                    </svg>
-                    <span className="sr-only">Loading...</span>
-                  </div>
-                ) : (
-                  <>
-                  <h2 id={styles._H2} className="text-left text-stone-400 font-avant_garde_bold">Sign Up to Bimcopilot</h2>
-
-
-                    <button
-                      type="button"
-                      className={styles.googleCustomButton}
-                      onClick={() => startGoogleLogin()}
-                    >
-                      <span className={styles.googleIcon} aria-hidden="true" />
-                      <span>Sign in with Google</span>
-                    </button>
-
-                    <form onSubmit={handleSubmit}>
-
-
-                      <div id={styles.BREAKLINE}>
-                        <div id={styles.LINE_1}
-                          style={{
-                            width: "100%",
-                            height: "1px",
-                          }}>
-                        </div>
-
-                        <div id={styles.TEXT}>
-                          <h5 className=" text-stone-400 ... ">or</h5>
-                        </div>
-
-                        <div id={styles.LINE_2}
-                          style={{
-                            width: "100%",
-                            height: "1px",
-                          }}>
-                        </div>
-                      </div>
-                      <br />
-
-                      {["email", "userName", "password", "confirmPassword"].map((field) => (
-                        <div key={field} className="mb-4">
-                          <label htmlFor={field} className="text-left text-stone-400 font-avant_garde_bold" id={styles._H3}>
-                            {field.replace(/([A-Z])/g, " $1").replace(/^./, (str) => str.toUpperCase())}
-                          </label>
-
-                          <input
-                            type={field.includes("password") ? "password" : "text"}
-                            id={field}
-                            name={field}
-                            value={formData[field]}
-                            onChange={handleChange}
-                            className="mt-1 border rounded-md"
-                            required
-                            style={{ width: "100%", height: "40px", padding: "15px" }}
-                          />
-                        </div>
-                      ))}
-
-                      <Button variant="outline"
-                        id={styles.FORM_BUTTON} type="submit" disabled={isLoading}>
-                        Create an Account
-                      </Button>
-                    </form>
-
-
-                  </>
-                )}
-              </div>
+              <SignupForm />
             </GoogleOAuthProvider>
           </div>
         </div>
